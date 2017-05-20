2b37123ca7a7c7138db2cb25f1b7970c440b3882
B Kleinen <kleinen@infrastructure.de>
2017-05-20 23:45:31 +0200

    use id for associated object in attributes_for

--- spec/controllers/user_comments_controller_spec.rb
+++ spec/controllers/user_comments_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe UserCommentsController, :type => :controller do
   describe "GET #new" do
 
     it 'assigns @comment' do
-      get :new, xhr: true, format: :js
+      get :new, xhr: true, format: :json
       expect(assigns(:comment)).to be_a_new(UserComment)
     end
   end
@@ -23,22 +23,22 @@ RSpec.describe UserCommentsController, :type => :controller do
 
     it 'creates a new UserComment' do
       expect {
-        post :create, xhr: true, user_comment: attributes_for(:user_comment, internship_id: @internship)
+        post :create, xhr: true, format: :js, user_comment: attributes_for(:user_comment, internship_id: @internship.id)
       }.to change(UserComment, :count).by(1)
     end
 
     it 'assigns @answer' do
-      post :create, xhr: true, user_comment: attributes_for(:user_comment, internship_id: @internship)
+      post :create, xhr: true, format: :js, user_comment: attributes_for(:user_comment, internship_id: @internship.id)
       expect(assigns(:answer)).to be_a_new(Answer)
     end
 
     it 'assigns @user_comments' do
-      post :create, xhr: true, user_comment: attributes_for(:user_comment, internship_id: @internship)
+      post :create, xhr: true, format: :js, user_comment: attributes_for(:user_comment, internship_id: @internship.id)
       expect(assigns(:user_comments)).to eq([@internship.user_comments.first])
     end
 
     it 'assigns @new_comment' do
-      post :create, xhr: true, user_comment: attributes_for(:user_comment, internship_id: @internship)
+      post :create, xhr: true, format: :js, user_comment: attributes_for(:user_comment, internship_id: @internship.id)
       expect(assigns(:new_comment)).to be_a_new(UserComment)
     end
 
@@ -65,7 +65,7 @@ RSpec.describe UserCommentsController, :type => :controller do
 
     context 'given incorrect parameters' do
       it 'refuses ot update the user_comment' do
-        put :update, xhr: true, format: :js, id: @user_comment, user_comment: attributes_for(:user_comment, body: nil)
+        put :update, xhr: true, format: :json, id: @user_comment, user_comment: attributes_for(:user_comment, body: nil)
         @user_comment.reload
         expect(@user_comment.body).to eq("Foo")
       end
@@ -88,12 +88,12 @@ RSpec.describe UserCommentsController, :type => :controller do
     end
 
     it 'assigns @internship' do
-      delete :destroy, id: @user_comment
+      delete :destroy, id: @user_comment, format: :js
       expect(assigns(:internship)).to eq(@user_comment.internship)
     end
 
     it 'assigns @user_comments' do
-      delete :destroy, id: @user_comment
+      delete :destroy, id: @user_comment, format: :js
       expect(assigns(:user_comments)).to eq([])
     end
   end
