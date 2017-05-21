1bf993da07baeccadd0f8f7b6764c7efb4e2d760
B Kleinen <kleinen@infrastructure.de>
2017-05-21 20:23:53 +0200

    regenerated and merged active_admin assets

--- app/assets/javascripts/active_admin.js
+++
@@ -1,4 +0,0 @@
-// TBD Update ActiveAdmin: uncomment the following line (by moving the = towards the slash)
-//  require active_admin/base
-//= require chosen-jquery
-//= require_tree ./active_admin
