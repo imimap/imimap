11ff861e1e150308b503f8e4fe18a2e127d1b545
B Kleinen <kleinen@infrastructure.de>
2017-05-21 19:13:45 +0200

    removed generated null: false to not change an existing migration

--- db/migrate/20130516091020_devise_create_admin_users.rb
+++ db/migrate/20130516091020_devise_create_admin_users.rb
@@ -19,7 +19,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
       t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
+      t.integer  :sign_in_count, default: 0
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
