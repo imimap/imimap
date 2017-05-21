71b1fad984dcc02ce44f8fb21ad7e15f18b003d5
B Kleinen <kleinen@infrastructure.de>
2017-05-21 12:55:09 +0200

    rake rails:update

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,10 +1,4 @@
-ImiMaps::Application.configure do
-
-  #IMI-Maps specific
-  config.allow_concurrency = false
-  LdapAuthentication.configure(mode: :test)
-  #IMI-Maps specific
-
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
@@ -20,7 +14,7 @@ ImiMaps::Application.configure do
 
   # Configure static asset server for tests with Cache-Control for performance.
   config.serve_static_assets  = true
-  config.static_cache_control = "public, max-age=3600"
+  config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
@@ -42,4 +36,10 @@ ImiMaps::Application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #IMI-Maps specific
+  config.allow_concurrency = false
+  LdapAuthentication.configure(mode: :test)
+  #IMI-Maps specific
+
 end
