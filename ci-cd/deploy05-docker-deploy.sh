817c6974b0d4b5fec13b80815c0eccb67fef62af
B Kleinen <kleinen@infrastructure.de>
2017-05-25 20:16:54 +0200

    where is deploy

--- ci-cd/deploy05-docker-deploy.sh
+++ ci-cd/deploy05-docker-deploy.sh
@@ -7,15 +7,16 @@ echo "Deploying image for environment: $DEPLOYMENT_ENVIRONMENT with tag $DEPLOYM
 
 export set DEPLOMENT_USER=deployer
 if [ $DEPLOYMENT_ENVIRONMENT == "staging" ]; then
-  export set DEPLOYMENT_HOST="imi-map-staging.f4.htw-berlin.de"
+  export DEPLOYMENT_HOST="imi-map-staging.f4.htw-berlin.de"
 fi
 if [ $DEPLOYMENT_ENVIRONMENT == "production" ]; then
-  export set DEPLOYMENT_HOST="imi-map-production.f4.htw-berlin.de"
+  export DEPLOYMENT_HOST="imi-map-production.f4.htw-berlin.de"
 fi
-
-echo "copying docker-compose file"
+echo "in dir"
+pwd
+echo "copying docker-compose file to $DEPLOYMENT_HOST"
 scp -i id_rsa_$DEPLOYMENT_ENVIRONMENT -o StrictHostKeyChecking=no .docker/$DEPLOYMENT_ENVIRONMENT/docker-compose-$DEPLOYMENT_ENVIRONMENT.yml $DEPLOMENT_USER@$DEPLOYMENT_HOST:~
 echo "sshing to $DEPLOYMENT_HOST and calling docker-compose"
-ssh  -i id_rsa_$DEPLOYMENT_ENVIRONMENT -o StrictHostKeyChecking=no $DEPLOMENT_USER@$DEPLOYMENT_HOST "export TAG=$DEPLOYMENT_TAG; docker-compose -f ~/docker-compose-$DEPLOYMENT_ENVIRONMENT.yml up -d "
+ssh  -i id_rsa_$DEPLOYMENT_ENVIRONMENT -o StrictHostKeyChecking=no $DEPLOMENT_USER@$DEPLOYMENT_HOST "export TAG=$DEPLOYMENT_TAG; echo "TAG: $TAG"; docker-compose -f ~/docker-compose-$DEPLOYMENT_ENVIRONMENT.yml up -d "
 
 echo "end $0"
