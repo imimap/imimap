b26808887a02c40c4696c301576e8295dac313ce
B Kleinen <kleinen@infrastructure.de>
2017-06-03 10:14:20 +0200

    test for search page de/en done, does not show error

--- spec/features/internal_area/search_spec.rb
+++ spec/features/internal_area/search_spec.rb
@@ -1,26 +1,24 @@
-
 require 'rails_helper'
 
-feature "Detailed Search Page " do
-  before :each do
-    @user = create(:user)
-  end
-    scenario "should have content " do
-      login_with(@user)
-        visit "/de/internships"
-        page.should have_content("Lebenshaltungskosten")
-        page.should have_content("Gehalt")
+describe "Detailed Search Page " do
+  I18n.available_locales.each do | locale |
+  context "in locale #{locale}" do
+    before :each do
+      I18n.locale = locale
+    end
+    #context "with valid user credentials" do
+      before :each do
+        @user = create(:user)
+      end
 
-        flunk "continue here"
-      #  click_link "English"
-      #  page.should have_content("Cost of living")
-      #  page.should have_content("salary")
-      #  page.should have_content(I18n.t('results.found'))
-      #  select('SS 13', :from => 'semester')
-      #  select('Brazil', :from => 'Choose a country')
-      #  select('United Kingdom', :from => 'country')
-      #  select('SS 12', :from => 'Choose a semester')
-      #  click_button "submit"
-      #  click_button "reset_chosen"
+    it "should have title and arbitrary search criteria" do
+      login_with(@user)
+        visit internships_path(locale: locale)
+        save_and_open_page
+        page.should have_content(t('search.title'))
+        page.should have_content(t('internships.attributes.living_costs'))
      end
+  # end
+ end
+end
 end
