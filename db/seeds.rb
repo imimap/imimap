1632e7da906a4db7d33908b4f5c4cadd4df5d98b
ViviZa <vivian.zaddach@gmail.com>
2017-06-21 14:01:51 +0200

    improve test data : start and end dates of internship

--- db/seeds.rb
+++ db/seeds.rb
@@ -160,20 +160,24 @@ n=1
   r_p_s = rand(PaymentState.count)+1
   r_i_s = rand(InternshipState.count)+1
 
-  internship = Internship.new(title: "Awesome Developer#{n}", salary: r_salary, internship_rating_id: iR.id, working_hours: r_work, living_costs: r_living, company_id: company.id, student_id: student.id, semester_id: semester.id, start_date: Time.at(rand*Time.now.to_f).to_date, end_date: Time.at(rand*Time.now.to_f).to_date, operational_area: Orientation.offset(r_o).first,
+  min_date = Time.now - 8.years
+  max_date = Time.now - 1.year
+  endDate = rand(min_date..max_date)
+
+  internship = Internship.new(title: "Awesome Developer#{n}", salary: r_salary, internship_rating_id: iR.id, working_hours: r_work, living_costs: r_living, company_id: company.id, student_id: student.id, semester_id: semester.id, start_date: min_date, end_date: endDate, operational_area: Orientation.offset(r_o).first,
       tasks: "a"*r_a, orientation_id: r_o, supervisor_name: "b"*r_b, supervisor_email: "e"*r_e,
     registration_state_id: r_r_s, contract_state_id: r_c_s, report_state_id: r_re_s, certificate_state_id: r_ce_s,
     payment_state_id: r_p_s, internship_state_id: r_i_s, comment: "p"*r_p, reading_prof_id: reading_prof_id, certificate_to_prof: Time.at(rand*Time.now.to_f).to_date,
     certificate_signed_by_prof: Time.at(rand*Time.now.to_f).to_date, certificate_signed_by_internship_officer: Time.at(rand*Time.now.to_f).to_date)
   s = rand(5)+1
- 	ary = []
-	s.times do
-		ary << rand(10)+1
-	end
-	internship.programming_languages = ProgrammingLanguage.where(:id => ary.uniq)
-	internship.save
+  ary = []
+  s.times do
+    ary << rand(10)+1
+  end
+  internship.programming_languages = ProgrammingLanguage.where(:id => ary.uniq)
+  internship.save
 
-	n+=1
+  n+=1
 
 end
 
