da5b55b599c3757f7454c7d3e8aa275d83a60090
Daniil Stepanov <danistep@mail.ru>
2017-06-28 15:03:47 +0200

    working(hopefully) viewtest

--- spec/views/internships/show.html.erb_spec.rb
+++ spec/views/internships/show.html.erb_spec.rb
@@ -1,14 +1,32 @@
 require 'spec_helper'
 
-describe 'internship/show.html.erb' do
+
+describe 'internships/show.html.erb' do
+  before do
+    @user = create(:user)
+    view.stub(:current_user).and_return(@user)
+    I18n.locale = locale
+  end
+
   context 'displayed in English'
-    it 'it shows internship correctly' do
-      let(:locale) {:en}
-      internship = build(:internship)
+  let(:locale) {:en}
+  it 'it shows internship correctly' do
+      user = create(:user)
+      internship = create(:internship)
+      internship.user = user
+      company = create(:company)
+
+      generic = [Internship.new, Internship.new ]
+      assign(:company, company)
+      assign(:other_internships, generic )
+
+      assign(:comment, UserComment.new)
+      assign(:user_comments, internship.user_comments)
+      assign(:internship, internship)
       render
 
-      rendered.should contain('1')
-      rendered.should contain('Intership is less than 4 weeks')
+      rendered.should have_content('1')
+      rendered.should have_content('Intership is less than 4 weeks')
       render.should have_content('Total amount of weeks')
       render.should have_content('Validation status')
   end
