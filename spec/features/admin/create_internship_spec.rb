a95f5f8359d89e5e070fa2d0f9a01bfd56250bda
B Kleinen <kleinen@infrastructure.de>
2017-07-06 16:56:38 +0200

    added example test

---
+++ spec/features/admin/create_internship_spec.rb
@@ -0,0 +1,21 @@
+
+require 'rails_helper'
+
+describe "ActiveAdmin create Internship" do
+    before :each do
+      @admin_user = create :admin_user
+      login_as(@admin_user, :scope => :admin_user)
+      I18n.locale = "de"
+    end
+    describe "pages" do
+      it "index" do
+        visit "admin/internships"
+      #  save_and_open_page
+        expect(page).to have_content "Internships"
+        click_button "New Internship"
+        expect(page).to have_content "New Internship"
+        # or: visit 'admin/internships/new?locale=en'
+      end
+    end
+
+end
