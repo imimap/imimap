0e6f39c76061e130f24df6ccf051a2298bba3cb9
Anna Bespalov <depmo@bes.local>
2017-07-09 19:00:31 +0200

    InternshipStatisticChart added

--- app/controllers/internship_statistic_controller.rb
+++ app/controllers/internship_statistic_controller.rb
@@ -1,8 +1,5 @@
 class InternshipStatisticController < ApplicationController
-  def show
-    @internships = Internship.all
-    @companies = @internships.collect(&:company)
-    @countries = @companies.collect(&:country)
-    @internships = @internships.where(:companies => {:country => params[:country]}) if params[:country].present?
+  def index
+	    @internships = Internship.all
   end
 end
