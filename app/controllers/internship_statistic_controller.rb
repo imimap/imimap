7224414695ac6f530cf72246b43ac541b9bf8119
Anna Bespalov <depmo@bes.local>
2017-07-09 16:18:20 +0200

    InternshipStatisticChart added, not completed

--- app/controllers/internship_statistic_controller.rb
+++ app/controllers/internship_statistic_controller.rb
@@ -1,5 +1,8 @@
-class StatisticController < ApplicationController
+class InternshipStatisticController < ApplicationController
   def show
     @internships = Internship.all
+    @companies = @internships.collect(&:company)
+    @countries = @companies.collect(&:country)
+    @internships = @internships.where(:companies => {:country => params[:country]}) if params[:country].present?
   end
 end
