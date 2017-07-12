d542b8808c0cde85d65c40111a53db91ceb0ab9f
Daniil Stepanov <danistep@mail.ru>
2017-07-12 10:21:24 +0200

    modified users controller to user our devise strategy instead of forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,27 @@ class UsersController < ApplicationController
   before_filter :check_existing_user, only: [:new, :create]
 
   def new
-    @user_creation_form = UserCreationForm.new(session[:enrolment_number])
-    respond_to do |format|
-      format.html
-    end
+    @user = User.new
+    # old code
+    # @user_creation_form = UserCreationForm.new(session[:enrolment_number])
+    # respond_to do |format|
+    #   format.html
+    # end
   end
 
   def create
-    @user_creation_form = UserCreationForm.new(session[:enrolment_number])
-    if @user_creation_form.submit(params[:user_creation_form])
-      #UserMailer.registration_confirmation(@user).deliver
-      session[:user_id] = @user_creation_form.id
-      redirect_to overview_index_path, notice: t('sign_up')
-    else
-      render :new
-    end
+    @user = User.new
+    @user.save!
+
+    # old code
+    # @user_creation_form = UserCreationForm.new(session[:enrolment_number])
+    # if @user_creation_form.submit(params[:user_creation_form])
+    #   #UserMailer.registration_confirmation(@user).deliver
+    #   session[:user_id] = @user_creation_form.id
+    #   redirect_to overview_index_path, notice: t('sign_up')
+    # else
+    #   render :new
+    # end
   end
 
   def show
