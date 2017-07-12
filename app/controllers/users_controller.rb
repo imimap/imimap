ff6b052f1de8fa25127a77b5851a4ee01378bb11
Daniil Stepanov <danistep@mail.ru>
2017-07-12 15:40:52 +0200

    Users controller with devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_filter :check_existing_user, only: [:new, :create]
 
   # def new
-  #   @user = User.new
+    @user = User.new
   #   # old code
   #   # @user_creation_form = UserCreationForm.new(session[:enrolment_number])
   #   # respond_to do |format|
@@ -11,20 +11,20 @@ class UsersController < ApplicationController
   #   # end
   # end
   #
-  # def create
-  #   @user = User.new()
-  #   @user.save!
-  #
-  #   # old code
-  #   # @user_creation_form = UserCreationForm.new(session[:enrolment_number])
-  #   # if @user_creation_form.submit(params[:user_creation_form])
-  #   #   #UserMailer.registration_confirmation(@user).deliver
-  #   #   session[:user_id] = @user_creation_form.id
-  #   #   redirect_to overview_index_path, notice: t('sign_up')
-  #   # else
-  #   #   render :new
-  #   # end
-  # end
+  def create
+    @user = User.new()
+    @user.save!
+
+    # old code
+    # @user_creation_form = UserCreationForm.new(session[:enrolment_number])
+    # if @user_creation_form.submit(params[:user_creation_form])
+    #   #UserMailer.registration_confirmation(@user).deliver
+    #   session[:user_id] = @user_creation_form.id
+    #   redirect_to overview_index_path, notice: t('sign_up')
+    # else
+    #   render :new
+    # end
+  end
 
   def show
     @user = User.find(params[:id])
