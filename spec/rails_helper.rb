2025b494ca31d1f732913edf2a038981cd7f3e62
Daniil Stepanov <danistep@mail.ru>
2017-07-16 15:54:40 +0200

    added feature tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,8 @@ RSpec.configure do |config|
   # https://github.com/plataformatec/devise
     config.include Devise::Test::ControllerHelpers, type: :controller
     config.include Devise::Test::ControllerHelpers, type: :view
+    config.include Devise::Test::IntegrationHelpers, type: :feature
+
 
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
