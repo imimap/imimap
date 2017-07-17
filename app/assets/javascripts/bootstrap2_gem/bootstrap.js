213607913be9383e227f988f8008fcd5d14817ec
Thomas Sweet <tsweetberlin@gmail.com>
2017-07-17 16:44:06 +0200

    Bootstrap update (#99)
    * new user in seeds
    
    * removed bootstrap-sass gem
    
    * added stylesheets and javascripts from gem
    
    * removed gem bootstrap-sass and replaced it with local bootstrap2 files
    
    * updated bootstrap gem to bootstrap3, started refactoring the scss files
    
    * boostrap gems updated and bootstrap-custom working
    
    * fixed conflicts with master

---
+++ app/assets/javascripts/bootstrap2_gem/bootstrap.js
@@ -0,0 +1,13 @@
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-transition
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-affix
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-alert
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-button
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-carousel
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-collapse
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-dropdown
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-modal
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-scrollspy
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-tab
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-tooltip
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-popover
+//= require ../../../../vendor/assets/javascripts/bootstrap/bootstrap-typeahead
