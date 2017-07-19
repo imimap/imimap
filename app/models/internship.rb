9aabf5f3b954c415ed056b0e9f707d2478276954
ViviZa <vivian.zaddach@gmail.com>
2017-07-19 11:53:58 +0200

    change return values of validation method

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -68,11 +68,11 @@ class Internship < ActiveRecord::Base
     valText = ""
     case weeksToValidate
       when 0..4
-        valText = A 
+        valText = "A" 
       when 4..17,5
-         valText = B
+         valText = "B"
        else
-        valText = C
+        valText = "C"
     end
     return valText;
 
