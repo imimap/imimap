6bd2ecc492b582b53a091356be8a52e230d9625d
B Kleinen <kleinen@infrastructure.de>
2017-07-24 00:21:34 +0200

    draw version from environment variables only

--- ci-cd/create-version-info.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export set VERSIONFILE="app/views/version/_version.html.erb"
-
-echo "IMI-Maps @" > "$VERSIONFILE"
-echo $IMIMAPS_ENVIRONMENT >> "$VERSIONFILE"
-echo $RAILS_ENV >> "$VERSIONFILE"
-git rev-parse --short HEAD >> "$VERSIONFILE"
-echo "TAG: <%= ENV['TAG'] %>" >> "$VERSIONFILE"
