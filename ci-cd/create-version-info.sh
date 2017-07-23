5f451ff3ebfb3282751ab0875a07f0cb6749c1c5
B Kleinen <kleinen@infrastructure.de>
2017-07-23 23:59:13 +0200

    version-info

--- ci-cd/create-version-info.sh
+++ ci-cd/create-version-info.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-export set VERSIONFILE="app/views/sessions/_version.html.erb"
+export set VERSIONFILE="app/views/version/_version.html.erb"
 
 echo "IMI-Maps @" > "$VERSIONFILE"
 echo $IMIMAPS_ENVIRONMENT >> "$VERSIONFILE"
 echo $RAILS_ENV >> "$VERSIONFILE"
 git rev-parse --short HEAD >> "$VERSIONFILE"
+echo "TAG: <%= ENV['TAG'] %>" >> "$VERSIONFILE"
