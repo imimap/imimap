d27727897e60f2f0899d91ee63bd061f79c98811
B Kleinen <kleinen@infrastructure.de>
2017-07-24 11:42:27 +0200

    used longer password in seed fixes #118

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
-echo "Production entrypoint"
+echo "Development entrypoint"
 
 # test if postgresql is up
 while ! nc -z postgresql 5432; do sleep 1; done
 
-cd /usr/src/app 
+cd /usr/src/app \
+  && ci-cd/create-version-info.sh \
+  && bundle exec rake db:migrate \
+  && bundle exec rake db:seed
 exec "$@"
