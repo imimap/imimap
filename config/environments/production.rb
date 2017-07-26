ee4ce0ac3df02b0a83e5bf5e920943f58f1b7b5d
Daniel Stepanov <danistep@mail.ru>
2017-07-26 16:35:49 +0200

    try to forse ssl on assets in staging (#137)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,10 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # Try to use ssl for everything on production
+  config.force_ssl = true
+
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
