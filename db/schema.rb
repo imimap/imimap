3c17712ab2cad03fe28b40de330cefcbba8494f1
Zainab Tariq <zainabtariq12@gmail.com>
2017-07-26 20:52:43 +0200

    Int registration (#139)
    * Started new feature
    
    * Admin can mark an internship as accepted
    
    * registration state added in database, added test
    
    * revert schema change
    
    * added test
    
    * fixed error handling according to tutorial on  (#76)
    
    * fixed error handling according to tutorial on https://mattbrictson.com/dynamic-rails-error-pages, fixes#45
    
    * restart travis
    
    * Internship statistics (#100)
    
    * add gem, controller and view for statistics
    
    * add route and includes in application layout
    
    * InternshipStatisticChart added, not completed
    
    * InternshipStatisticChart added
    
    * add 3 more statistics
    
    * modify headline of statistic
    
    * select internship in specific semester is possible, select coutries in this specific semester
    
    * statistic shows now internships per semester per country
    
    * rendering works by choosing semester
    
    * add Statistik as a button to the menu
    
    * restart travis build
    
    * restart travis build 2.0
    
    * Issue63 (#101)
    
    * show max 5 internships
    
    * fixed quicksearches view to use correct partial, quickview now shows 5 elements
    
    * Bootstrap update (#99)
    
    * new user in seeds
    
    * removed bootstrap-sass gem
    
    * added stylesheets and javascripts from gem
    
    * removed gem bootstrap-sass and replaced it with local bootstrap2 files
    
    * updated bootstrap gem to bootstrap3, started refactoring the scss files
    
    * boostrap gems updated and bootstrap-custom working
    
    * fixed conflicts with master
    
    * Unnecessaryroutes (#102)
    
    * delete unused routes
    
    * comment out test for unused route
    
    * added new Registration states using migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719063910) do
+ActiveRecord::Schema.define(version: 20170723095341) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "resource_id",   limit: 255, null: false
