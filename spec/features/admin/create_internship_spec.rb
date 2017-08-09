f2f2c018c0aaa2c9b5db82cb2e3b33111b302bba
Samira <samira.hassan@student.htw-berlin.de>
2017-08-09 17:17:17 +0200

    working on admin tests

--- spec/features/admin/create_internship_spec.rb
+++ spec/features/admin/create_internship_spec.rb
@@ -4,13 +4,14 @@ require 'rails_helper'
 describe "ActiveAdmin create Internship" do
     before :each do
       @admin_user = create :admin_user
-      login_as(@admin_user, :scope => :admin_user)
+    # login_as(@admin_user, :scope => :admin_user)
+      sign_in(@admin_user)
       I18n.locale = "de"
     end
     describe "pages" do
       it "index" do
         visit "admin/internships"
-      #  save_and_open_page
+        save_and_open_page
         expect(page).to have_content "Internships"
       #  click_button "New Internship"
       #  expect(page).to have_content "New Internship"
