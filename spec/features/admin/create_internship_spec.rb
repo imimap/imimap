a4739d2b82e701aa7365481c9a3993d49323962a
Samira <samira.hassan@student.htw-berlin.de>
2017-08-09 18:04:59 +0200

    added translation to test

--- spec/features/admin/create_internship_spec.rb
+++ spec/features/admin/create_internship_spec.rb
@@ -6,13 +6,15 @@ describe "ActiveAdmin create Internship" do
       @admin_user = create :admin_user
     # login_as(@admin_user, :scope => :admin_user)
       sign_in(@admin_user)
-      I18n.locale = "de"
+      I18n.locale = "en"
     end
     describe "pages" do
       it "index" do
-        visit "admin/internships"
+      #  visit "admin/internships"
+        visit admin_internships_path
+        expect(I18n.locale).to eq :de
         save_and_open_page
-        expect(page).to have_content "Internships"
+        expect(page).to have_content I18n.t("activerecord.models.internship.other")
       #  click_button "New Internship"
       #  expect(page).to have_content "New Internship"
         # or: visit 'admin/internships/new?locale=en'
