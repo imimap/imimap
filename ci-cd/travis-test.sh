00fe296fa28b04b67500c25303989ec9e5e7d91d
B Kleinen <kleinen@infrastructure.de>
2017-08-10 17:14:48 +0200

    added port again

--- ci-cd/travis-test.sh
+++ ci-cd/travis-test.sh
@@ -4,10 +4,11 @@ echo "$0: starting build for IMIMAPS_ENVIRONMENT ${IMIMAPS_ENVIRONMENT}"
 if [ "$IMIMAPS_ENVIRONMENT" == "docker" ]; then
   echo "lsof -i tcp:5432"
   lsof -i tcp:5432
+
   docker-compose up -d
+
   echo "done building and starting image"
-  #echo "starting tests in docker image"
-  #docker-compose run imimap "./ci-cd/travis-test-commands.sh"
+  echo "starting tests in docker image"
 
   docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:create RAILS_ENV=test
   docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:migrate RAILS_ENV=test
