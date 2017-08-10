61b68ef4efe66af880d2d50d3bd61a6c6883b73f
B Kleinen <kleinen@infrastructure.de>
2017-08-10 17:08:49 +0200

    run containers as demon

--- ci-cd/travis-test.sh
+++ ci-cd/travis-test.sh
@@ -4,8 +4,8 @@ echo "$0: starting build for IMIMAPS_ENVIRONMENT ${IMIMAPS_ENVIRONMENT}"
 if [ "$IMIMAPS_ENVIRONMENT" == "docker" ]; then
   echo "lsof -i tcp:5432"
   lsof -i tcp:5432
-  docker-compose up
-  echo "done building image"
+  docker-compose up -d
+  echo "done building and starting image"
   #echo "starting tests in docker image"
   #docker-compose run imimap "./ci-cd/travis-test-commands.sh"
 
