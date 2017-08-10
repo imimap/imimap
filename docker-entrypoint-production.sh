7d568c59c9dbab7941fdee8754b409903c43a7d8
B Kleinen <kleinen@infrastructure.de>
2017-08-10 02:59:35 +0200

    replaced docker-tool

--- docker-entrypoint-production.sh
+++ docker-entrypoint-production.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -eo pipefail
-echo "Production entrypoint"
+echo "Development entrypoint"
 
 # test if postgresql is up
 while ! nc -z postgresql 5432; do sleep 1; done
 
-cd /usr/src/app 
+cd /usr/src/app \
+  && bundle exec rake db:migrate \
+  && bundle exec rake db:seed
 exec "$@"
