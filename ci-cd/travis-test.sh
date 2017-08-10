7922a1359e6db7caff5a832b89cebaab1f0fd3fe
B Kleinen <kleinen@infrastructure.de>
2017-08-10 20:49:40 +0200

    little things clearing up

--- ci-cd/travis-test.sh
+++ ci-cd/travis-test.sh
@@ -2,21 +2,31 @@
 echo "$0: starting build for IMIMAPS_ENVIRONMENT ${IMIMAPS_ENVIRONMENT}"
 
 if [ "$IMIMAPS_ENVIRONMENT" == "docker" ]; then
-  echo "lsof -i tcp:5432"
-  lsof -i tcp:5432
 
+  echo "--------- netstat -nlp | grep 5432"
+  netstat -nlp | grep 5432
   docker-compose up -d
 
-  echo "done building and starting image"
-  echo "starting tests in docker image"
+  if [ $? != 0 ]; then
+    echo "ERROR: docker-compose up -d FAILED"
+    exit 1
+  else
+    echo "--------- docker exec -ti postgresql-dev netstat -nlp | grep 5432"
+    docker exec -ti postgresql-dev netstat -nlp | grep 5432
 
-  docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:create RAILS_ENV=test
-  docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:migrate RAILS_ENV=test
-  docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:migrate:status RAILS_ENV=test
-  docker exec -e RAILS_ENV=test -ti imimaps-dev rspec spec
+    echo "done building and starting image"
+    echo "starting tests in docker image"
 
+
+    docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:create RAILS_ENV=test
+    docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:migrate RAILS_ENV=test
+    docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:migrate:status RAILS_ENV=test
+    docker exec -e RAILS_ENV=test -ti imimaps-dev rspec spec
+  fi
 
 else
+  gem install bundler
+  bundle --version
   bundle install
   bundle exec rake db:migrate RAILS_ENV=test
   bundle exec rspec spec
