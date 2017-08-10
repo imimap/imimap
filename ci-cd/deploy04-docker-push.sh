056a04f2a4846c4a719b445789ee35e93ebe1ed0
B Kleinen <kleinen@infrastructure.de>
2017-08-10 03:14:44 +0200

    removed unused cicd-libraries and files

--- ci-cd/deploy04-docker-push.sh
+++
@@ -1,4 +0,0 @@
-echo "start: deploy04-docker-push.sh"
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push $DEPLOYMENT_DOCKER_ORGANISATION/$DEPLOYMENT_ENVIRONMENT:$DEPLOYMENT_TAG
-echo "end: deploy04-docker-push.sh"
