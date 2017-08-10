f1e93252e9bd6706b12247f274faa1e686d98102
B Kleinen <kleinen@infrastructure.de>
2017-08-10 17:02:53 +0200

    set RAILS_ENV via docker exec command

--- ci-cd/travis-test-commands.sh
+++ ci-cd/travis-test-commands.sh
@@ -3,4 +3,5 @@ echo $0
 echo "./ci-cd/travis-test-commands.sh"
 bundle exec rake db:create RAILS_ENV=test
 bundle exec rake db:migrate RAILS_ENV=test
+bundle exec rake db:status RAILS_ENV=test
 bundle exec rspec spec
--- ci-cd/travis-test.sh
+++ ci-cd/travis-test.sh
@@ -4,10 +4,17 @@ echo "$0: starting build for IMIMAPS_ENVIRONMENT ${IMIMAPS_ENVIRONMENT}"
 if [ "$IMIMAPS_ENVIRONMENT" == "docker" ]; then
   echo "lsof -i tcp:5432"
   lsof -i tcp:5432
-  docker-compose build
+  docker-compose up
   echo "done building image"
-  echo "starting tests in docker image"
-  docker-compose run imimap "./ci-cd/travis-test-commands.sh"
+  #echo "starting tests in docker image"
+  #docker-compose run imimap "./ci-cd/travis-test-commands.sh"
+
+  docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:create RAILS_ENV=test
+  docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:migrate RAILS_ENV=test
+  docker exec -e RAILS_ENV=test -ti imimaps-dev rake db:migrate:status RAILS_ENV=test
+  docker exec -e RAILS_ENV=test -ti imimaps-dev rspec spec
+
+
 else
   bundle install
   bundle exec rake db:migrate RAILS_ENV=test
