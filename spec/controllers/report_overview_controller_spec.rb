bec51aa3ee6093c53c050a205ad5ba014e56b4cf
kingmuscle <kristiawan.dion@gmail.com>
2017-08-12 17:59:21 +0200

    new report_overview cont test & delete old one (wrong test)

--- spec/controllers/report_overview_controller_spec.rb
+++ spec/controllers/report_overview_controller_spec.rb
@@ -1,32 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe ReportOverviewController, type: :controller do
+  render_views
+
+  before :each do
+    @internship = create :internship
+    @current_user = login
+  end
 
   describe "GET #index" do
 
 
-    it 'renders the index action correctly' do
+    it 'renders the index action' do
       get :index
       expect(response).to have_http_status(:success)
     end
 
     it 'assigns @internships' do
-      create :internship, completed: false
-
       get :index
       expect(assigns(:internships)).to eq([@internship])
     end
 
-    it 'assigns @companies' do
-      get :index
-      expect(assigns(:companies)).to eq([@internship].map(&:company))
-    end
 
-
-    it 'assigns @semesters' do
-      get :index
-      expect(assigns(:semesters)).to eq([@internship].map(&:semester))
-    end
 
   end
 
