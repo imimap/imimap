64b7622f77a3025e753d37ca1c8c7cd93a10097a
kingmuscle <kristiawan.dion@gmail.com>
2017-08-16 11:26:56 +0200

    add more style and update my internship status page

--- app/controllers/internship_status_controller.rb
+++ app/controllers/internship_status_controller.rb
@@ -1,19 +1,16 @@
 class InternshipStatusController < ApplicationController
-  respond_to :html, :json
+
   before_filter :authorize
 
 
 
   def index
 
-    # @internships = Internship.where(report_state_id: ReportState.second, semester_id: Semester.first)
+
 
     #@internship = Internship.where(user_id: current_user.id).last
 
     @internship = Internship.where(user_id: current_user).last
-
-
-
 
 
   end
