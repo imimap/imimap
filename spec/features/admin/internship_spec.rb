61f6e62abbb9f4900468f697a822f5c14b658da6
Samira <samira.hassan@student.htw-berlin.de>
2017-08-22 14:00:47 +0200

    Test internship_spec added, one failure so far, 'translation missing'

--- spec/features/admin/internship_spec.rb
+++ spec/features/admin/internship_spec.rb
@@ -1,1 +1,43 @@
+require 'rails_helper'
 
+describe "ActiveAdmin Internship CRUD" do
+  context "logged in" do
+    before :each do
+      @admin_user = create :admin_user
+      sign_in@admin_user
+      I18n.locale = "de"
+    end
+    describe "show internship" do
+      it "shows internship" do
+        internship = create(:internship)
+        visit admin_internship_path(internship)
+      #  save_and_open_page
+      #  expect(page).to have_content internship.end_date
+      #  expect(page).to have_content internship.operational_area
+      #  expect(page).to have_content internship.tasks
+      #  expect(page).to have_content internship.supervisor_name
+      #  expect(page).to have_content internship.supervisor_email
+      #  expect(page).to have_content internship.comment
+        expect(page).to have_content I18n.t("activerecord.models.start_date.other")
+        expect(page).to have_content I18n.t("activerecord.models.end_date.other")
+        expect(page).to have_content I18n.t("activerecord.models.tasks.other")
+        expect(page).to have_content I18n.t("activerecord.models.supervisor_email.other")
+        expect(page).to have_content I18n.t("activerecord.models.supervisor_name.other")
+        expect(page).to have_content I18n.t("activerecord.models.comment.other")
+      end
+    end
+  end
+  context "not logged in" do
+    describe "show internship" do
+      it "shows unauthenticated failure" do
+        I18n.locale = "de"
+        expected_text = I18n.t('devise.failure.admin_user.unauthenticated')
+        internship = create(:internship)
+        visit admin_internship_path(internship)
+
+        # need to add translation
+        # expect(page).to have_content expected_text
+      end
+    end
+  end
+end
