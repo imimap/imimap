233243849308c65f98963427f288fbd156efa23b
ViviZa <vivian.zaddach@gmail.com>
2017-09-04 12:14:20 +0200

    add real test data for programming languages

--- db/seed/06_programmingLanguages.rb
+++ db/seed/06_programmingLanguages.rb
@@ -1,7 +1,10 @@
 # ruby encoding: utf-8
-ProgrammingLanguage.destroy_all
-15.times do
-  ProgrammingLanguage.create!(
-    name: Faker::Job.key_skill
-  )
-end
+ProgrammingLanguage.where(name: "Java").first_or_create
+ProgrammingLanguage.where(name: "C++").first_or_create
+ProgrammingLanguage.where(name: "C").first_or_create
+ProgrammingLanguage.where(name: "C#").first_or_create
+ProgrammingLanguage.where(name: "Ruby").first_or_create
+ProgrammingLanguage.where(name: "PHP").first_or_create
+ProgrammingLanguage.where(name: "Javascript").first_or_create
+ProgrammingLanguage.where(name: "HTML/CSS").first_or_create
+ProgrammingLanguage.where(name: "Python").first_or_create
