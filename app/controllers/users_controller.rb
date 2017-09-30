7c0f2db589772a6c1bae0370b3f97823ce2ebf5a
Daniil Stepanov <danistep@mail.ru>
2017-09-30 21:57:17 +0200

    removed unused user actions edit/update and corresponding views/forms and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,28 +3,9 @@ class UsersController < ApplicationController
   before_filter :check_permission, only: [:new, :create]
   before_filter :check_existing_user, only: [:new, :create]
 
-  # def new
-    @user = User.new
-  #   # old code
-  #   # @user_creation_form = UserCreationForm.new(session[:enrolment_number])
-  #   # respond_to do |format|
-  #   #   format.html
-  #   # end
-  # end
-  #
   def create
     @user = User.new()
     @user.save!
-
-    # old code
-    # @user_creation_form = UserCreationForm.new(session[:enrolment_number])
-    # if @user_creation_form.submit(params[:user_creation_form])
-    #   #UserMailer.registration_confirmation(@user).deliver
-    #   session[:user_id] = @user_creation_form.id
-    #   redirect_to overview_index_path, notice: t('sign_up')
-    # else
-    #   render :new
-    # end
   end
 
   def show
@@ -33,23 +14,6 @@ class UsersController < ApplicationController
     @comments = @user.user_comments
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'Details were successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
 private
     def check_permission
