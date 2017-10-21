b29b21a1e55fbe270a5fab5c6c4a27cb63dbbe22
B Kleinen <kleinen@infrastructure.de>
2017-10-21 07:48:56 +0200

    rubocop

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,13 +1,15 @@
-class AdminUser < ActiveRecord::Base
+# frozen_string_literal: true
 
+# User for the activeadmin part. Deprecated. TBD: check if it can be deleted
+class AdminUser < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  validates :email, :presence => true
-  validates :password, :presence => true
-  validates :password_confirmation, :presence => true
-#  validates :remember_me, :presence => true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 
+  # validates :remember_me, :presence => true
 end
