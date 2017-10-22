d88d91c360aa94188b29634d35441eaeda41f775
B Kleinen <kleinen@infrastructure.de>
2017-10-22 22:29:18 +0200

    fixed leaflet map

--- app/controllers/startpage_controller.rb
+++ app/controllers/startpage_controller.rb
@@ -1,50 +1,13 @@
+# frozen_string_literal: true
+
+# StartPage is the landing page for the app
 class StartpageController < ApplicationController
+  include MapHelper
   skip_before_action :authenticate_user!
   layout 'startpage'
 
   def new
-    if user_signed_in?
-      redirect_to overview_index_url
-    end
-
-    #@company_location_json = Company.pluck(:name, :latitude, :longitude).to_json.html_safe
-    @company_location_json_raw = Company.pluck(:name, :latitude, :longitude)
-    @company_location_json_raw.each do |x|
-        x[0] = x[0].gsub('\'', ' ')
-      end
-     @company_location_json = @company_location_json_raw.to_json.html_safe
-
-
-
-    @internships = Internship.includes(:company, :semester, :orientation, :programming_languages).where(completed: true).order('created_at DESC')
+    redirect_to overview_index_url if user_signed_in?
+    @company_location_json = company_locations_json
   end
-
-  # I think we don't need any of that stuff anymore
-  # def create
-  #   user = User.find_by_email(params[:email])
-  #   #
-  #   if user && user.authenticate(params[:password])
-  #     session[:user_id] = user.id
-  #     redirect_to overview_index_url, notice: "Logged in!"
-  #     #if params[:remember_me]
-  #     #  cookies.permanent[:auth_token] = user.auth_token
-  #     #else
-  #     #  cookies[:auth_token] = user.auth_token
-  #     #end
-  #     #redirect_to overview_index_url, :notice => "Logged in!"
-  #
-  #   else
-  #     #flash.now.alert = "Email or password is invalid"
-  #     flash[:alert] = t("msg.invalid")
-  #
-  #     redirect_to :back
-  #   end
-  # end
-  #
-  # def destroy
-  #   startpage[:user_id] = nil
-  #   #cookies.delete(:auth_token)
-  #   redirect_to root_url, :alert =>  t("msg.logout")
-  # end
-
- end
+end
