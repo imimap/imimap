7f2ad245404aaf5dac7ec509f344f4ce757f010d
B Kleinen <kleinen@infrastructure.de>
2017-10-22 22:40:44 +0200

    always run the tests...

--- spec/controllers/overview_controller_spec.rb
+++ spec/controllers/overview_controller_spec.rb
@@ -27,40 +27,5 @@ RSpec.describe OverviewController, :type => :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
-
-    it 'assigns @internships' do
-      create :internship, completed: false
-
-      get :index
-      expect(assigns(:internships)).to eq([@internship])
-    end
-
-    it 'assigns @companies' do
-      get :index
-      expect(assigns(:companies)).to eq([@internship].map(&:company))
-    end
-
-    it 'assigns @programming_languages' do
-      programming_language = create(:programming_language)
-      @internship.programming_languages << programming_language
-
-      get :index
-      expect(assigns(:programming_languages)).to eq([programming_language])
-    end
-
-    it 'assigns @semesters' do
-      get :index
-      expect(assigns(:semesters)).to eq([@internship].map(&:semester))
-    end
-
-    it 'assigns @orientations' do
-      get :index
-      expect(assigns(:orientations)).to eq([[@internship.orientation.name, @internship.orientation.id]])
-    end
-
-    it 'assigns @countries' do
-      get :index
-      expect(assigns(:countries)).to eq([@internship.company.country])
-    end
   end
 end
