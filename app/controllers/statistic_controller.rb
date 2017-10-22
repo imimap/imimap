bfef4cd83a21f0d4e0cade80ae7dc58905471b2e
B Kleinen <kleinen@infrastructure.de>
2017-10-22 14:55:38 +0200

    fixed statistic

---
+++ app/controllers/statistic_controller.rb
@@ -0,0 +1,15 @@
+class StatisticController < ApplicationController
+  before_filter :authorize
+  def overview
+    @internships = Internship.all
+    @companies = Company.all
+    @semesters = Semester.all
+
+    if params[:semester_id] == nil
+      @semester_id = Semester.last.id
+    else
+      @semester_id = params[:semester_id]
+    end
+  end
+
+end
