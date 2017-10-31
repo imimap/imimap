69034c79e23dacc78f9188f93e2990abc44ea054
B Kleinen <kleinen@infrastructure.de>
2017-10-31 18:33:46 +0100

    put active admin routes in locale scope, test cleanup

---
+++ spec/features/admin/activeadmin_i18n_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'I18n in ActiveAdmin specs' do
+  before :each do
+    @admin_user = create :admin_user
+    sign_in(@admin_user)
+  end
+  after :all do
+    I18n.locale = :en
+  end
+  describe 'path_helper' do
+    it 'contains locale in path' do
+      p = admin_internships_path
+      expect(p).to eq('/admin/internships')
+    end
+    it 'sets locale to de on default' do
+      visit admin_internships_path
+      expect(I18n.locale).to eq :de
+      expect(admin_internships_path).to eq('/admin/internships')
+      # save_and_open_page
+      expect(page).to have_content I18n.t('activerecord.models.internship.other')
+    end
+    it 'path sets locale en' do
+      visit '/en/admin/internships'
+      expect(I18n.locale).to eq :en
+    end
+    it 'path sets locale de' do
+      visit '/de/admin/internships'
+      expect(I18n.locale).to eq :de
+      expect(admin_internships_path).to eq ('/admin/internships')
+    end
+  end
+end
--- spec/features/admin/internship_spec.rb
+++ spec/features/admin/internship_spec.rb
@@ -1,42 +1,36 @@
 require 'rails_helper'
 
-describe "ActiveAdmin Internship CRUD" do
-  context "logged in" do
+describe 'ActiveAdmin Internship CRUD' do
+  context 'logged in' do
     before :each do
       @admin_user = create :admin_user
       sign_in@admin_user
-      I18n.locale = "de"
+      I18n.locale = 'de'
     end
-    describe "show internship" do
-      it "shows internship" do
+    describe 'show internship' do
+      it 'shows internship' do
         internship = create(:internship)
-        visit admin_internship_path(internship)
-      #  save_and_open_page
-      #  expect(page).to have_content internship.end_date
-      #  expect(page).to have_content internship.operational_area
-      #  expect(page).to have_content internship.tasks
-      #  expect(page).to have_content internship.supervisor_name
-      #  expect(page).to have_content internship.supervisor_email
-      #  expect(page).to have_content internship.comment
-        expect(page).to have_content I18n.t("activerecord.attributes.internship.start_date")
-        expect(page).to have_content I18n.t("activerecord.attributes.internship.end_date")
-        expect(page).to have_content I18n.t("activerecord.attributes.internship.tasks")
-        expect(page).to have_content I18n.t("activerecord.attributes.internship.supervisor_email")
-        expect(page).to have_content I18n.t("activerecord.attributes.internship.supervisor_name")
-        expect(page).to have_content I18n.t("activerecord.attributes.internship.comment")
+        visit admin_internship_path(id: internship)
+        expect(page).to have_content I18n.t('activerecord.attributes.internship.start_date')
+        expect(page).to have_content I18n.t('activerecord.attributes.internship.end_date')
+        expect(page).to have_content I18n.t('activerecord.attributes.internship.tasks')
+        expect(page).to have_content I18n.t('activerecord.attributes.internship.supervisor_email')
+        expect(page).to have_content I18n.t('activerecord.attributes.internship.supervisor_name')
+        expect(page).to have_content I18n.t('activerecord.attributes.internship.comment')
       end
     end
   end
-  context "not logged in" do
-    describe "show internship" do
-      it "shows unauthenticated failure" do
-        I18n.locale = "de"
-        expected_text = I18n.t('devise.failure.admin_user.unauthenticated')
+  context 'not logged in' do
+    describe 'show internship' do
+      it 'shows unauthenticated failure' do
+        I18n.locale = :en
+        expected_text = I18n.t('devise.failure.unauthenticated')
+        # expected_text = I18n.t('devise.failure.admin_user.unauthenticated')
         internship = create(:internship)
-        visit admin_internship_path(internship)
+        visit admin_internship_path(locale: :en, id: internship)
 
         # need to add translation
-        # expect(page).to have_content expected_text
+        expect(page).to have_content expected_text
       end
     end
   end
