d8d2384973a815e1d3ffc095741adb1e48a43ef1
B Kleinen <kleinen@infrastructure.de>
2017-11-07 08:16:40 +0100

    3.3 Halting Callback Chains via throw(:abort) - removed unnecessary before callback in user, user.rb cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,42 +1,30 @@
 # frozen_string_literal: true
 
-#
+# Represents a user in the imi-map.
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  #has_secure_password
 
   attr_accessible :email, :password, :password_confirmation, :publicmail,
                   :mailnotif, :student_id, :remember_me
-
 
   validates :email, presence: true
   validates :password, presence: true, length: { minimum: 5 }
   validates :student, presence: true
 
   belongs_to :student
-  has_many :user_comments, :dependent => :destroy
-  has_many :favorites, :dependent => :destroy
-  has_many :notifications, :dependent => :destroy
-  has_many :read_lists, :dependent => :destroy
-  has_many :finish_lists, :dependent => :destroy
+  has_many :user_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+  has_many :read_lists, dependent: :destroy
+  has_many :finish_lists, dependent: :destroy
 
   def name
     "#{student.first_name} #{student.last_name}"
   end
 
   def enrolment_number
-    student.enrolment_number if student
+    return nil unless student
+    student.enrolment_number
   end
-
-  before_create { generate_token(:auth_token) }
-
-  def generate_token(column)
-    begin
-      self[column] = SecureRandom.urlsafe_base64
-    end while User.exists?(column => self[column])
-  end
-
 end
