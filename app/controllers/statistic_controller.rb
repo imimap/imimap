5428e1ab4fba468fcaf353b3e000d2b129578c9c
B Kleinen <kleinen@infrastructure.de>
2017-11-10 14:51:24 +0100

    fixed statistic

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -1,15 +1,15 @@
 class StatisticController < ApplicationController
 
   def overview
-    @internships = Internship.all
-    @companies = Company.all
-    @semesters = Semester.all
-
-    if params[:semester_id] == nil
-      @semester_id = Semester.last.id
-    else
-      @semester_id = params[:semester_id]
+    unless (Semester.count == 0 || Internship.count == 0)
+      if params[:semester_id] == nil
+        @semester_id = Semester.last.id
+      else
+        @semester_id = params[:semester_id].to_i
+      end
+      @semester = Semester.find(@semester_id)
+      @semester_options = Semester.all.map { |s| [s.name, s.id] }
+      @internships = Internship.joins(:company).where(semester_id: @semester_id).group(:country).count
     end
   end
-
 end
