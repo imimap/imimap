83e198871ea9dcff808366501f702135a2ed5371
B Kleinen <kleinen@infrastructure.de>
2017-11-10 16:30:21 +0100

    fixed test

--- spec/features/internal_area/login_spec.rb
+++ spec/features/internal_area/login_spec.rb
@@ -1,29 +1,27 @@
 # frozen_string_literal: true
 
-#
 require 'rails_helper'
 
-describe "Student Login" do
-  I18n.available_locales.each do | locale |
-  context "in locale #{locale}" do
-    before :each do
-      I18n.locale = locale
-    end
-    context "with valid user credentials" do
+describe 'Student Login' do
+  I18n.available_locales.each do |locale|
+    context "in locale #{locale}" do
       before :each do
-        @user = create(:user)
+        I18n.locale = locale
       end
+      context 'with valid user credentials' do
+        before :each do
+          @user = create(:user)
+        end
 
-      it "should proceed to log in" do
-        visit internships_path
-        fill_in "user_email",  :with => @user.email
-        fill_in "user_password",  :with => @user.password
-        click_on('Log in')
+        it 'should proceed to log in and back to original page' do
+          visit internships_path
 
-        # needs to be switched off to pass travis
-        # expect(page).to have_content I18n.t('header.exchange')
+          fill_in 'user_email', with: @user.email
+          fill_in 'user_password', with: @user.password
+          click_on('Log in')
+          expect(page).to have_content I18n.t('search.title')
+        end
       end
     end
- end
- end
- end
+  end
+end
