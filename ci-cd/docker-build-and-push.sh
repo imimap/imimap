5b234f40bd8724d0714dcfd6ac81b3236d80e312
B Kleinen <kleinen@infrastructure.de>
2018-02-16 17:08:43 +0100

    major cleanup

--- ci-cd/docker-build-and-push.sh
+++ ci-cd/docker-build-and-push.sh
@@ -6,12 +6,7 @@ export DEPLOYMENT_ENVIRONMENT=staging
 . ./ci-cd/deploy01-settings.sh
 . ./ci-cd/deploy00-echo-settings.sh
 
-# if [ $DEPLOYMENT_SHOULD_RUN != "true" ]; then
-#   echo "***** SKIPPING BUILD AND PUSH: DEPLOYMENT_SHOULD_RUN $DEPLOYMENT_SHOULD_RUN *****"
-#   echo "end $0"
-#   exit 0
-# fi
-
+echo "---------------------- calling docker build ----------------------"
 
 docker build -f Dockerfile.production -t imimap/imimap:$DEPLOYMENT_TAG .
 
