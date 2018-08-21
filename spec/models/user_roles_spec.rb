483a9d8c18c38a066daaaaaacb1464d9cda5623a
B Kleinen <kleinen@infrastructure.de>
2018-08-22 01:52:55 +0200

    orderd roles

--- spec/models/user_roles_spec.rb
+++ spec/models/user_roles_spec.rb
@@ -6,10 +6,15 @@ require 'rails_helper'
 describe 'User roles' do
   it 'has a list of valid roles' do
     expect(User::ROLES.size).to eq 4
-
   end
   it 'nonexistent roles cannot be assigned' do
     u = User.new
     expect {u.role = :no_role}.to raise_error(ArgumentError)
+  end
+  it 'saves role' do
+    u = User.create(email: 'bla@blub.de',
+                    password: 'geheim12',
+                    password_confirmation: 'geheim12')
+    expect(u.user?).to eq true
   end
 end
