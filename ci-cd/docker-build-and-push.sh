c06a2243adf0f75c320c9ee69ef207428b095c1a
Barne Kleinen <kleinen@htw-berlin.de>
2018-08-22 16:08:16 +0200

    Fixed docker build after upgrade to Rails 5.2
    RAILS_MASTER_KEY needs to be available during docker image build time via build-arg/ARG rather than as a ENV variable.

--- ci-cd/docker-build-and-push.sh
+++ ci-cd/docker-build-and-push.sh
@@ -8,13 +8,23 @@ export DEPLOYMENT_ENVIRONMENT=staging
 
 echo "---------------------- calling docker build ----------------------"
 
-docker build -f Dockerfile.production -t imimap/imimap:$DEPLOYMENT_TAG .
+docker pull imimap/imimap:$DEPLOYMENT_TAG
 
 return_code=$?
 if [ $return_code != 0 ]; then
-  echo "FAILED: docker build"
-  exit $return_code
+  echo "IMAGE with tag does not yet exist; building"
+  echo ${imimap/imimap:$DEPLOYMENT_TAG}
+
+  docker build -f Dockerfile.production --build-arg RAILS_MASTER_KEY=$RAILS_MASTER_KEY  -t imimap/imimap:$DEPLOYMENT_TAG .
+  return_code=$?
+  if [ $return_code != 0 ]; then
+    echo "FAILED: docker build"
+    exit $return_code
+  fi
+
 fi
+
+
 
 echo "--- docker images ---"
 docker images
