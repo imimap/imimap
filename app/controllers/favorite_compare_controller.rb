d051f388d3f40e90569a986d81c1a468a36ea774
B Kleinen <kleinen@infrastructure.de>
2018-08-22 09:52:13 +0200

    patched codacy issue

--- app/controllers/favorite_compare_controller.rb
+++ app/controllers/favorite_compare_controller.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+# TBD Search - Favourites are switched off and should be
+# restaurated with the search
 class FavoriteCompareController < ApplicationController
   def index
-    @internships = if params[:favorite_ids]
-                     Internship.find(params[:favorite_ids])
-                   else
-                     []
-                   end
+    @internships = []
+    # @internships = if params[:favorite_ids]
+    #                  Internship.find(params[:favorite_ids])
+    #                else
+    #                  []
+    #                end
 
     respond_to do |format|
       format.js { render layout: false }
