8c977c7ef128c6cf46985a18ae193f66555c65eb
B Kleinen <kleinen@infrastructure.de>
2018-08-27 12:39:02 +0200

    rubocop company_addresses

--- spec/factories/company_addresses.rb
+++ spec/factories/company_addresses.rb
@@ -3,23 +3,23 @@
 # Companies
 FactoryBot.define do
   factory :company_address, class: CompanyAddress do
-    street 'MyString'
-    zip 'MyString'
-    city 'MyString'
-    country 'MyString'
+    street { 'MyString' }
+    zip { 'MyString' }
+    city { 'MyString' }
+    country { 'MyString' }
   end
 
   factory :company_address_htw, class: CompanyAddress do
-    city 'Berlin'
-    country 'Germany'
-    street 'Wilhelminenhofstr. 75 A'
-    zip '12459'
+    city { 'Berlin' }
+    country { 'Germany' }
+    street { 'Wilhelminenhofstr. 75 A' }
+    zip { '12459' }
   end
 
   factory :company_is24, class: CompanyAddress do
-    city 'Berlin'
-    country 'Germany'
-    street 'Andreasstr. 10'
-    zip '10243'
+    city { 'Berlin' }
+    country { 'Germany' }
+    street { 'Andreasstr. 10' }
+    zip { '10243' }
   end
 end
