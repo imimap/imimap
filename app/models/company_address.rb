6719ea3d6f7d026b4b7ba15b53bfd3cbced9a6d4
B Kleinen <kleinen@infrastructure.de>
2018-08-27 12:16:21 +0200

    bk: started to fix factories

--- app/models/company_address.rb
+++ app/models/company_address.rb
@@ -3,3 +3,14 @@ class CompanyAddress < ApplicationRecord
     has_many :internships
     validates_presence_of :company
 end
+
+# city { 'Berlin' }
+# country { 'Germany' }
+# street { 'Wilhelminenhofstr. 75 A' }
+# zip { '12459' }
+
+
+# city { 'Berlin' }
+# country { 'Germany' }
+# street { 'Andreasstr. 10' }
+# zip { '10243' }
--- spec/factories/company_addresses.rb
+++ spec/factories/company_addresses.rb
@@ -1,8 +1,25 @@
+# frozen_string_literal: true
+
+# Companies
 FactoryBot.define do
-  factory :company_address do
-    street "MyString"
-    zip "MyString"
-    city "MyString"
-    country "MyString"
+  factory :company_address, class: CompanyAddress do
+    street 'MyString'
+    zip 'MyString'
+    city 'MyString'
+    country 'MyString'
+  end
+
+  factory :company_address_htw, class: CompanyAddress do
+    city 'Berlin'
+    country 'Germany'
+    street 'Wilhelminenhofstr. 75 A'
+    zip '12459'
+  end
+
+  factory :company_is24, class: CompanyAddress do
+    city 'Berlin'
+    country 'Germany'
+    street 'Andreasstr. 10'
+    zip '10243'
   end
 end
