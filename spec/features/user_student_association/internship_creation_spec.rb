8dbb7b08dda7f2d5387f0b2bef45fee631fa92ce
B Kleinen <kleinen@infrastructure.de>
2018-08-30 11:53:01 +0200

    added failing test case for student association

--- spec/features/user_student_association/internship_creation_spec.rb
+++ spec/features/user_student_association/internship_creation_spec.rb
@@ -2,21 +2,37 @@
 
 require 'rails_helper'
 
+def sign_in_with(enrolment_number:)
+  visit root_path
+  fill_in 'user_email', with: "s#{enrolment_number}@htw-berlin.de"
+  fill_in 'user_password', with: 'geheim'
+  click_on I18n.t('devise.sessions.submit')
+  expect(page).to have_content t('devise.sessions.signed_in')
+end
+
 describe 'Internship creation and editing' do
+  before (:each) do
+    @ldap_mock = ldap_mock = instance_double('Net::LDAP')
+    LDAPHTWAdapter.substitute_netldap(mock: ldap_mock)
+    allow(ldap_mock).to receive(:bind).and_return(true)
+  end
+
   context 'first time - no user present' do
-    before :each do
-    end
+
     it 'user is created' do
-      expect(User.count).to eq 0
       expect do
-        visit root_path
-        fill_in 'user_email', with: 's054321@htw-berlin.de'
-        fill_in 'user_password', with: 'geheim'
-        click_on I18n.t('devise.sessions.submit')
-        expect(page).to have_content t('devise.sessions.signed_in')
-      end.to change{User.count}.by(1)
+        sign_in_with(enrolment_number: '54321')
+      end.to change { User.count }.by(1)
     end
-    it 'created user is associated with existing student object'
+
+    it 'created user is associated with existing student object' do
+      enrolment_number = '12121'
+      student = create(:student, enrolment_number: enrolment_number)
+      sign_in_with(enrolment_number: enrolment_number)
+      user = User.last
+      expect(user.email).to eq "s#{enrolment_number}@htw-berlin.de"
+      expect(user.student).to eq student
+    end
     it 'student object is created if not present'
   end
 
