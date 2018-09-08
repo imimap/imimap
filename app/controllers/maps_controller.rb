acac632fefa03f76c39e90e448f3b4c4bd7162d3
B Kleinen <kleinen@infrastructure.de>
2018-09-08 09:58:17 +0200

    map working!

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,10 +3,11 @@
 # Controller for all map views
 class MapsController < ApplicationController
   skip_authorization_check only: [:start_page]
-  include MapHelper
+  include MapsHelper
   def start_page
     @map_view = true
-    @company_location_json = company_locations_json
+    @company_addresses = CompanyAddress.where.not(latitude: nil)
+    @company_location_json = company_locations_json(company_addresses: @company_addresses)
   end
 
   def debug; end
--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
+# Helper Methods for Maps Views
 module MapsHelper
+  def company_locations_json(company_addresses:)
+    companies = company_addresses.pluck(:city, :country, :latitude, :longitude)
+    companies = companies.reject { |c| c.include?(nil) }
+    companies = companies.map { |c| ["#{c[0]}, #{c[1]}", c[2], c[3]] }
+    company_location_json_raw = companies.uniq { |c| c[0] }
+    company_location_json_raw.each do |x|
+      x[0] = x[0].tr('\'', ' ')
+    end
+    company_location_json_raw.to_json.html_safe
+  end
 end
