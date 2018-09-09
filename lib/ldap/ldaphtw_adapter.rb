fa1f3fa85f78b7c67a327e95d36bcb25246d7b34
B Kleinen <kleinen@infrastructure.de>
2018-09-09 12:32:01 +0200

    email match

--- lib/ldap/ldaphtw_adapter.rb
+++ lib/ldap/ldaphtw_adapter.rb
@@ -75,7 +75,7 @@ class LDAPHTWAdapter
   private
 
   def ldap_username
-    m = /\A(.*)@.*htw-berlin.de\z/.match(email)
+    m = /\A(.*)@.*htw-berlin.de\z/.match(email.strip)
     return m[1] if m
     Rails.logger.info("-- ldap -- email not valid #{email} ")
     nil
