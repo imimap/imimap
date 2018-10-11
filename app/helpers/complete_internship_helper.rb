f0267391292efa649c10c2b13fd4de63db10cb75
Barne Kleinen <barne.kleinen@htw-berlin.de>
2018-10-11 18:06:33 +0200

    rubocop cleanup

--- app/helpers/complete_internship_helper.rb
+++ app/helpers/complete_internship_helper.rb
@@ -1,39 +1,12 @@
+# frozen_string_literal: true
+
+# Composes all Internship Information in one Class to pass to the view
 module CompleteInternshipHelper
-  # frozen_string_literal: true
   def CompleteInternship.from(int)
     ci = CompleteInternship.new
-    if int.student.nil?
-      ci.first_name = '(no student)'
-      ci.last_name = '(no student)'
-      ci.enrolment_number = '(none)'
-    else
-      ci.first_name = int.student.first_name
-      ci.last_name = int.student.last_name
-      ci.enrolment_number = int.student.enrolment_number
-    end
-    ci.company = if int.company_v2.nil?
-                   '(no company)'
-                 else
-                   int.company_v2.name
-                 end
-    if int.company_address.nil?
-      ci.city = '(no city)'
-      ci.country = '(no country)'
-    else
-      ci.city = int.company_address.city
-      ci.country = int.company_address.country_name
-    end
-    ci.start_date = int.start_date
-    ci.end_date = int.end_date
-
-    if int.internship_state.name == "passed"
-    ci.internship_state = int.internship_state.name
-  else
-    contr = int.contract_state.name
-    reg = int.registration_state.name
-    cert = int.certificate_state.name
-    ci.internship_state = 'Contract: ' + contr + '; Registration: ' + reg + '; Certificate: ' + cert
-  end
+    ci.add_student_info(int)
+    ci.add_company_info(int)
+    ci.add_status_info(int)
     ci
   end
 
@@ -45,5 +18,4 @@ module CompleteInternshipHelper
       end
     end
   end
-
 end
