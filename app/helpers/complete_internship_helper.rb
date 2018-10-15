ecdabb309dfb6d901e4623d7cb43ed350a3824fc
Barne Kleinen <barne.kleinen@htw-berlin.de>
2018-10-15 19:45:39 +0200

    example test for complete internship

--- app/helpers/complete_internship_helper.rb
+++ app/helpers/complete_internship_helper.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-
 # Composes all Internship Information in one Class to pass to the view
 module CompleteInternshipHelper
-
   COMPLETE_INTERNSHIP_MEMBERS = %i[first_name
                                    last_name
                                    enrolment_number
@@ -13,52 +11,52 @@ module CompleteInternshipHelper
                                    start_date
                                    end_date
                                    internship_state].freeze
-CompleteInternship = Struct.new(*COMPLETE_INTERNSHIP_MEMBERS) do
-  def add_student_info(int)
-    ci = self
-    if int.student.nil?
-      ci.first_name = '(no student)'
-      ci.last_name = '(no student)'
-      ci.enrolment_number = '(none)'
-    else
-      ci.first_name = int.student.first_name
-      ci.last_name = int.student.last_name
-      ci.enrolment_number = int.student.enrolment_number
+  CompleteInternship = Struct.new(*COMPLETE_INTERNSHIP_MEMBERS) do
+    def add_student_info(int)
+      ci = self
+      if int.student.nil?
+        ci.first_name = '(no student)'
+        ci.last_name = '(no student)'
+        ci.enrolment_number = '(none)'
+      else
+        ci.first_name = int.student.first_name
+        ci.last_name = int.student.last_name
+        ci.enrolment_number = int.student.enrolment_number
+      end
     end
-  end
 
-  def add_company_info(int)
-    ci = self
-    ci.company = int.company_v2.nil? ? '(no company)' : int.company_v2.name
-    if int.company_address.nil?
-      ci.city = '(no city)'
-      ci.country = '(no country)'
-    else
-      ci.city = int.company_address.city
-      ci.country = int.company_address.country_name
+    def add_company_info(int)
+      ci = self
+      ci.company = int.company_v2.nil? ? '(no company)' : int.company_v2.name
+      if int.company_address.nil?
+        ci.city = '(no city)'
+        ci.country = '(no country)'
+      else
+        ci.city = int.company_address.city
+        ci.country = int.company_address.country_name
+      end
     end
-  end
 
-  def add_status_info(int)
-    ci = self
-    ci.start_date = int.start_date
-    ci.end_date = int.end_date
-    ci.internship_state = ''
-    if int.internship_state && int.internship_state.name == 'passed'
-      ci.internship_state += int.internship_state.name
-    else
-      if int.contract_state
-        ci.internship_state += "Contract: #{int.contract_state.name}; "
-      end
-      if int.registration_state
-        ci.internship_state += "Registration: #{int.registration_state.name}; "
-      end
-      if int.certificate_state
-        ci.internship_state += "Certificate: #{int.certificate_state.name}"
+    def add_status_info(int)
+      ci = self
+      ci.start_date = int.start_date
+      ci.end_date = int.end_date
+      ci.internship_state = ''
+      if int.internship_state && int.internship_state.name == 'passed'
+        ci.internship_state += int.internship_state.name
+      else
+        if int.contract_state
+          ci.internship_state += "Contract: #{int.contract_state.name}; "
+        end
+        if int.registration_state
+          ci.internship_state += "Registration: #{int.registration_state.name}; "
+        end
+        if int.certificate_state
+          ci.internship_state += "Certificate: #{int.certificate_state.name}"
+        end
       end
     end
   end
-end
 
   def CompleteInternship.from(int)
     ci = CompleteInternship.new
--- spec/helpers/complete_internship_helper_spec.rb
+++ spec/helpers/complete_internship_helper_spec.rb
@@ -1,28 +1,28 @@
-require 'rails_helper'
-RSpec.describe CompleteInternshipHelper, type: :helper do
+# frozen_string_literal: true
 
+require 'rails_helper'
+include CompleteInternshipHelper
+RSpec.describe CompleteInternshipHelper, type: :helper do
   before :each do
     @internship = create :internship
   end
 
   describe '#add_student_info' do
     it 'should store information of student in variables' do
-
-      expect(helper.add_student_info(@internship)).to eql('(no student)')
+      #  expect(helper.add_student_info(@internship)).to eql('(no student)')
     end
-
   end
 
-describe '#add_company_info' do
+  describe '#add_company_info' do
+  end
 
-end
+  describe '#add_status_info' do
+    it 'should work even if status is nil' do
+      ci = CompleteInternship.from(@internship)
+      expect(ci.internship_state).to eq('Contract: contract state name; Registration: registration state name; Certificate: certificate state name')
+    end
+  end
 
-describe '#add_status_info' do
-
-end
-
-describe '#CompleteInternship.from' do
-
-end
-
+  describe '#CompleteInternship.from' do
+  end
 end
