2bf5deee44a15f7fd2c008952632cbbcef62fa40
B Kleinen <kleinen@infrastructure.de>
2018-12-08 16:36:32 +0100

    added dump creation to makefile

--- ci-cd/create-dump.sh
+++
@@ -1,3 +0,0 @@
-scp ci-cd/create-dump-on-machine.sh deployer@imi-map-production.f4.htw-berlin.de:create-dump.sh
-ssh deployer@imi-map-production.f4.htw-berlin.de "./create-dump.sh"
-scp deployer@imi-map-production.f4.htw-berlin.de:imi-map-$(date +%Y-%m-%d).pgdump .
