8487bdd8744f831756b7b9840d36b8227a2bd6ed
B Kleinen <kleinen@infrastructure.de>
2018-12-18 20:12:24 +0100

    fixed 2 line length issues

--- spec/features/admin/admin_user/admin_user_spec.rb
+++ spec/features/admin/admin_user/admin_user_spec.rb
@@ -29,10 +29,12 @@ describe 'ActiveAdmin Admin User' do
         expect(page).not_to have_content 'NoMethodError'
         # save_and_open_page # um zu sehen was man sieht...
         # das funktioniert nicht weil es ein button ist:
-        # expect(page).to have_content t('helpers.submit.update', model: User.model_name.human)
+        # expect(page).to
+        # have_content t('helpers.submit.update', model: User.model_name.human)
         old_email = @user.email
         # nb: there is no translation yet for User.human_attribute_name(:email)
-        fill_in User.human_attribute_name(:email), with: 'testmail@htw-berlin.de'
+        fill_in User.human_attribute_name(:email),
+                with: 'testmail@htw-berlin.de'
         click_on t('helpers.submit.update', model: User.model_name.human)
         expect(page).to have_content 'testmail@htw-berlin.de'
         expect(page).not_to have_content old_email
