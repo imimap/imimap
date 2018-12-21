288c5112e8c8de450dfc39c25387a448a5c95818
B Kleinen <kleinen@infrastructure.de>
2018-12-21 01:24:42 +0100

    der codacy-report kam stunden später, hier sind die non-conflicting fixes

--- db/seed/01_semester.rb
+++ db/seed/01_semester.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Semester.destroy_all
-(2009..Date.today.year+1).to_a.each do |year|
+(2009..Date.today.year + 1).to_a.each do |year|
   Semester.create(sid: year + 0.1)
   Semester.create(sid: year + 0.2)
 end
