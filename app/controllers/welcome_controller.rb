880a0bcc9a0045feaa5188e521e15bb90bf4cf1d
B Kleinen <kleinen@infrastructure.de>
2019-02-28 09:05:08 +0100

    class comment for WelcomeController

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Controller with welcoming name for hosting the help page and possibly
+# other static pages.
 class WelcomeController < ApplicationController
   skip_authorization_check
   # renders the help site
