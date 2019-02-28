fcdeebead9967a2b460381dee9b49d8c5bcca4af
Barne Kleinen <barne.kleinen@htw-berlin.de>
2019-02-28 21:40:33 +0100

    semester is now association in CompleteInternship (#322)
    * CompleteInternship#semester needs to be an association

--- spec/factories/complete_internships.rb
+++ spec/factories/complete_internships.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :complete_internship do
-    semester { 'WS 2019/20' }
+    semester
     semester_of_study { 4 }
     aep { true }
     passed { false }
--- spec/views/complete_internships/index.html.erb_spec.rb
+++ spec/views/complete_internships/index.html.erb_spec.rb
@@ -5,27 +5,22 @@ require 'rails_helper'
 RSpec.describe 'complete_internships/index', type: :view do
   before(:each) do
     assign(:complete_internships, [
-             CompleteInternship.create!(
-               semester: 'WS 2019/20',
-               semester_of_study: 4,
-               aep: false,
-               passed: false
-             ),
-             CompleteInternship.create!(
-               semester: 'SS 2019',
-               semester_of_study: 5,
-               aep: true,
-               passed: false
-             )
+             create(:complete_internship,
+                    semester: build(:ws2019),
+                    semester_of_study: 4),
+             create(:complete_internship,
+                    semester: build(:ss2019),
+                    semester_of_study: 5, aep: false)
+
            ])
   end
 
   it 'renders a list of complete_internships' do
     render
-    assert_select 'tr>td', text: 'WS 2019/20'.to_s, count: 1
-    assert_select 'tr>td', text: 'SS 2019'.to_s, count: 1
+    assert_select 'tr>td', text: 'WS 19/20'.to_s, count: 1
+    assert_select 'tr>td', text: 'SS 19'.to_s, count: 1
     assert_select 'tr>td', text: 4.to_s, count: 1
-    assert_select 'tr>td', text: 4.to_s, count: 1
+    assert_select 'tr>td', text: 5.to_s, count: 1
     assert_select 'tr>td', text: true.to_s, count: 1
     assert_select 'tr>td', text: false.to_s, count: 3
   end
