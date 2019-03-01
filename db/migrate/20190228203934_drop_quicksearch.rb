0b0e664e3908d1df5d5c0e65fd3cc9ddf8cdbf72
Barne Kleinen <barne.kleinen@htw-berlin.de>
2019-03-01 01:47:40 +0100

    made last migration revertible (#325)
    * correct schema.rb
    
    * added empty rollback for drop quicksearches
    
    * added proper down for drop quicksearches

--- db/migrate/20190228203934_drop_quicksearch.rb
+++ db/migrate/20190228203934_drop_quicksearch.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class DropQuicksearch < ActiveRecord::Migration[5.2]
-  def change
+  def up
     drop_table :quicksearches
+  end
+
+  def down
+    create_table :quicksearches
   end
 end
