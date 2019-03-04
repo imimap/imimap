8efaa8b8147b3cc44a6591b4ea695fb8d61f6727
Barne Kleinen <barne.kleinen@htw-berlin.de>
2019-03-04 21:21:01 +0100

    gem updates (#330)
    * update
    
    * updated specs to work with FactoryBot 5

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,12 +4,15 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   before :each do
-    @answer = build :answer
+    @answer = create :answer
   end
 
   context 'given a valid Answer' do
     it 'can be saved with all required attributes present' do
-      expect(@answer.save).to be_truthy
+      # FactoryBot 5 factories now inherit their parent's create strategy
+      # https://www.rubydoc.info/gems/factory_bot/file/GETTING_STARTED.md
+      # had to simplify this test:
+      expect(@answer.errors).to be_empty
     end
   end
 
