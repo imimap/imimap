968b3da0f69fc8435bb78332b6123c5179ab6f26
B Kleinen <kleinen@infrastructure.de>
2019-03-06 02:16:01 +0100

    codacy: removed unscoped call

--- app/controllers/complete_internships_controller.rb
+++ app/controllers/complete_internships_controller.rb
@@ -64,7 +64,10 @@ class CompleteInternshipsController < ApplicationResourceController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_complete_internship
-    @complete_internship = CompleteInternship.find(params[:id])
+    accessible = current_user.accessible_complete_internships
+                  
+    @complete_internship =
+                           accessible.find(params[:id])
     @semester_name = @complete_internship.semester.try(:name)
   end
 
