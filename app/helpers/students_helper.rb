a2dd173b6c0208a96d2acfe9e947d21bb9e2108d
Julia Zamaitat <j.zamaitat@gmail.com>
2019-03-07 13:31:08 +0100

    Removed rubocop offense due to unneccessary method.

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -6,10 +6,4 @@ module StudentsHelper
     s = student
     "#{s.enrolment_number}, #{s.last_name}, #{s.first_name}"
   end
-
-  def datepicker_input(form, field)
-    content_tag :td, data: { :provide => 'datepicker', 'date-format' => 'yyyy-mm-dd', 'date-autoclose' => 'true' } do
-      form.text_field field, class: 'form-control', placeholder: 'YYYY-MM-DD'
-    end
-  end
 end
