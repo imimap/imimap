c2a8caf2bac46902b54ccc00eac5775363947dfa
B Kleinen <kleinen@infrastructure.de>
2019-03-28 08:53:57 +0100

    fixed autoreplacement in migrations

--- db/migrate/20190306131344_add_privateemail_to_students.rb
+++ db/migrate/20190306131344_add_privateemail_to_students.rb
@@ -1,5 +1,5 @@
 class AddPrivateemailToStudents < ActiveRecord::Migration[5.2]
   def change
-    add_column :students, :private_email, :string
+    add_column :students, :privateemail, :string
   end
 end
--- db/migrate/20190328072831_rename_student_private_email.rb
+++ db/migrate/20190328072831_rename_student_private_email.rb
@@ -1,5 +1,5 @@
 class RenameStudentPrivateEmail < ActiveRecord::Migration[5.2]
   def change
-     rename_column :students, :private_email, :private_email
+     rename_column :students, :privateemail, :private_email
   end
 end
