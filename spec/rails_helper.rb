944e0c49208f566b3f431d702223de8034768760
B Kleinen <kleinen@infrastructure.de>
2019-03-28 08:37:43 +0100

    check for migrations before running tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,8 @@ require_relative './spec_helper'
 require_relative './geocoder_mock'
 require 'rspec/rails'
 
+ActiveRecord::Migration.maintain_test_schema!
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
