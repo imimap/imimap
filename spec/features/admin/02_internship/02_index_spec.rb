f9aff3cbd08c4ba4771e0dff1f1b0bc7a607f2c6
B Kleinen <kleinen@infrastructure.de>
2019-04-01 20:15:35 +0200

    show admin internship index without complete_internship

--- spec/features/admin/02_internship/02_index_spec.rb
+++ spec/features/admin/02_internship/02_index_spec.rb
@@ -22,4 +22,11 @@ describe 'ActiveAdmin index internship' do
     expect(page).to have_content @internship.company_v2.name
     expect(page).to have_content @internship.semester.name
   end
+  it 'renders with an unassociated internship' do
+    internship2 = create(:unassociated_internship)
+    visit admin_internships_path
+    expect(page).to have_content I18n.t('active_admin.no_student')
+    expect(page).to have_content internship2.company_v2.name
+    expect(page).to have_content internship2.semester.name
+  end
 end
