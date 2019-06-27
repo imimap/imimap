33ec9870ec6b40f67a45dad028a9daba2d3ea4fd
lotteunckell <lotteunckell@gmail.com>
2019-06-27 14:40:25 +0200

    added test for saving reading prof. closes #368

--- spec/features/student_views/complete_internship_spec.rb
+++ spec/features/student_views/complete_internship_spec.rb
@@ -25,13 +25,49 @@ describe 'Complete Internship' do
         end
 
         it 'should create a new partial internship' do
+          create(:semester)
           visit my_internship_path
           click_link(t('internships.createYourInternship'))
           click_on t('save')
           click_on t('complete_internships.new_tp0')
           expect(page).to have_field('Semester')
           click_on t('save')
-          # not finished yet
+          expect(page).to have_content('Tuennes Schael')
+          expect(page).to have_content(
+            t('complete_internships.aep.number')
+          )
+          expect(page).to have_content(
+            t('complete_internships.parcial_internships.number')
+          )
+          expect(page).to have_content(
+            t('complete_internships.ci.number')
+          )
+          expect(page).to have_content(Semester.last.name)
+        end
+
+        # t("complete_internships.checklist.personal_details")
+        # t("complete_internships.checklist.internship_details")
+
+        it 'should save changes made in internship datails form
+            (example reading_prof selection)' do
+          create(:semester)
+          create(:reading_prof1)
+          create(:reading_prof2)
+          create(:reading_prof3)
+          visit my_internship_path
+          click_link(t('internships.createYourInternship'))
+          click_on t('save')
+          click_on t('complete_internships.new_tp0')
+          click_on t('save')
+          click_link(t('complete_internships.checklist.internship_details'))
+          expect(page).to have_content(
+            t('internships.attributes.which_professor_should_read')
+          )
+          select 'Prof. 3', from: 'internship_reading_prof_id'
+          click_on t('save')
+          click_link(t('complete_internships.checklist.internship_details'))
+          expect(page).to have_select('internship_reading_prof_id',
+                                      selected: 'Prof. 3')
         end
       end
     end
