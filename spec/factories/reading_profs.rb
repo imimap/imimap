26cfac1ca4c53b5102f3ea3a1dc86c0b97413ce1
lotteunckell <41839717+lotteunckell@users.noreply.github.com>
2019-07-01 19:15:07 +0200

    i368 - added test for saving reading prof selection (#376)
    * added test for saving reading prof. closes #368
    
    * removed unnecessary comments

--- spec/factories/reading_profs.rb
+++ spec/factories/reading_profs.rb
@@ -4,4 +4,16 @@ FactoryBot.define do
   factory :reading_prof do
     name { 'reading prof name' }
   end
+
+  factory :reading_prof1, class: ReadingProf do
+    name { 'Prof. 1' }
+  end
+
+  factory :reading_prof2, class: ReadingProf do
+    name { 'Prof. 2' }
+  end
+
+  factory :reading_prof3, class: ReadingProf do
+    name { 'Prof. 3' }
+  end
 end
