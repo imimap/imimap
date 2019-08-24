818291d027c9d1be092de624ca3ec27c59daaae3
B Kleinen <kleinen@infrastructure.de>
2019-08-24 17:57:30 +0200

    added tests and fixed bug, closes #409

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -28,11 +28,8 @@ class StudentsController < ApplicationResourceController
     @user = @student.user
     if @student.update_attributes(student_params)
       flash[:success] = 'Profil geupdated'
-      redirect_to @student.complete_internship
-      # TBD  #409 redirect_to @student
-    else
-      render 'show'
     end
+    render 'show'
   end
 
   private
