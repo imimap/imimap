67dbb1d6f97a26e7eaf04822a01629a9f9ad8d80
B Kleinen <kleinen@infrastructure.de>
2019-08-30 12:45:39 +0200

    first draft in application_record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,15 @@
 # Superclass for all Models
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.attributes_required_for_save
+    validators.select do |validator|
+      validator.is_a?(ActiveRecord::Validations::PresenceValidator)
+    end.map(&:attributes).flatten
+  end
+
+  def self.attributes_required_for_internship_application
+    @@attributes_required_for_internship_application
+  end
+
 end
