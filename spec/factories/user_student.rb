2e86ac02338cd3c404b18c5f695894f43fb8c2be
B Kleinen <kleinen@infrastructure.de>
2019-09-03 16:14:14 +0200

    wip

--- spec/factories/user_student.rb
+++ spec/factories/user_student.rb
@@ -44,4 +44,20 @@ FactoryBot.define do
       user.student ||= FactoryBot.build(:student, user: user)
     end
   end
+
+  factory :student_user_with_internship_company_wo_address, class: User do
+    email { 's012879@htw-berlin.de' }
+    password { 'geheim17' }
+    password_confirmation { 'geheim17' }
+    publicmail { false }
+    mailnotif { false }
+    role { :user }
+    after(:create) do |user|
+      student = user.student ||= FactoryBot.create(:student, user: user)
+      student.complete_internship ||= FactoryBot.create(
+        :complete_internship_w_fresh_internship,
+        student: student
+      )
+    end
+  end
 end
--- spec/features/student_views/company_address_spec.rb
+++ spec/features/student_views/company_address_spec.rb
@@ -3,40 +3,45 @@
 require 'rails_helper'
 
 describe 'Supply Company Details' do
-#I18n.available_locales.each do |locale|
-#  context "in locale #{locale}" do
-#      before :each do
-#        I18n.locale = locale
-#      end
-      context 'with student user and now Comany Information' do
-        before :each do
-          @user = create(:student_user_with_internship_company_wo_address)
-          login_as(@user)
-          create(:semester)
-          @company_name = 'The Backend Company'
-          ca = build(:plain_company_address)
-        end
+  # I18n.available_locales.each do |locale|
+  #  context "in locale #{locale}" do
+  #      before :each do
+  #        I18n.locale = locale
+  #      end
+  context 'with student user and now Comany Information' do
+    before :each do
+      @user = create(:student_user_with_internship_company_wo_address)
+      login_as(@user)
+      create(:semester)
+      @company_name = 'The Backend Company'
+      @ca = build(:plain_company_address, country: 'sy')
+    end
 
-        it 'should create company' do
-          visit my_internship_path
-          click_on t('complete_internships.checklist.company_details')
-          # fill_in :company_name_for_search, @company_name
-          save_and_open_page
-          fill_in :name, with: @company_name
-          click_on t('companies.continue2')
-          expect(page).to have_link(
-            @company_name
-          )
-          click_on @company_name
-          expect(page).to have_content('Neue Firma Erstellen')
-          click_on 'Weiter zur Adresse'
-          expect(page).to have_content('Erstelle eine neue Firmenaddresse')
-          @company_address.attributes_required_for_save.each do | field |
-            fill_in field, with:
-          end
-
-        end
+    it 'should create company' do
+      visit my_internship_path
+      click_on t('complete_internships.checklist.company_details')
+      # fill_in :company_name_for_search, @company_name
+      fill_in :name, with: @company_name
+      click_on t('companies.continue2')
+      expect(page).to have_link(
+        @company_name
+      )
+      click_on @company_name
+      expect(page).to have_content('Neue Firma Erstellen')
+      click_on 'Weiter zur Adresse'
+      expect(page).to have_content('Erstelle eine neue Firmenaddresse')
+      fields = CompanyAddress.attributes_required_for_save.dup
+      fields.delete(:company)
+      fields.delete(:country)
+      save_and_open_page
+      select @ca.country_name, from: :company_address_country
+      fields.each do |field|
+        fill_in "company_address_#{field}", with: @ca.send(field)
       end
-#    end
-#  end
+      click_button t('save')
+      save_and_open_page
+    end
+  end
+  #    end
+  #  end
 end
