ff15e7df2b37b05428e39271713d2da894942c95
B Kleinen <kleinen@infrastructure.de>
2019-10-13 10:03:53 +0200

    readme and seeds

--- db/seed/08_internships_companies_students_users.rb
+++ db/seed/08_internships_companies_students_users.rb
@@ -10,26 +10,30 @@ CompanyAddress.destroy_all
 
 # if users are created, the password is geheim12
 
-puts 'creating 10_001..10_020'
-(11_001..11_020).each do |er|
+range = 11_001..11_020
+puts "creating ##{range}"
+range.each do |er|
   create_student_with(enrolment_number: er,
                       with_internships: 1,
                       with_user: false)
 end
-puts 'creating 20_001..20_020'
-(12_001..12_020).each do |er|
+range = 12_001..12_020
+puts "creating ##{range}"
+range.each do |er|
   create_student_with(enrolment_number: er,
                       with_internships: 0,
                       with_user: true)
 end
-puts 'creating 30_001..30_020'
-(13_001..13_020).each do |er|
+range = 13_001..13_020
+puts "creating ##{range}"
+range.each do |er|
   create_student_with(enrolment_number: er,
                       with_internships: 0,
                       with_user: false)
 end
-puts 'creating 40_001..40_020'
-(14_001..14_020).each do |er|
+range = 40_001..40_02
+puts "creating ##{range}"
+range.each do |er|
   create_student_with(enrolment_number: er,
                       with_internships: 2,
                       with_user: true)
