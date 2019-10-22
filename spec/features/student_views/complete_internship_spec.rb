a6b825a3db76195d90f9f7af1c52edce17eb5061
Lotte <lotteunckell@gmail.com>
2019-10-22 16:07:11 +0200

    added test to catch hash being displayed, closes #455

--- spec/features/student_views/complete_internship_spec.rb
+++ spec/features/student_views/complete_internship_spec.rb
@@ -22,6 +22,7 @@ describe 'Complete Internship' do
           expect(page).to have_field('Fachsemester')
           click_on t('save')
           expect(page).to have_content(@user.name)
+          expect(page).not_to have_content('{false=>"still open", true=>"passed"}')
         end
 
         it 'should create a new partial internship' do
