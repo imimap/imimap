a6dfdb5ea265bdc72bcae30506ec95b26b3de8bd
Barne Kleinen <kleinen@infrastructure.de>
2019-10-27 13:00:11 +0100

    don't fail on failing db dump (#474)

--- ci-cd/deployment-steps-on-production-machine.sh
+++ ci-cd/deployment-steps-on-production-machine.sh
@@ -9,6 +9,15 @@ exit_on_error () {
       exit $sshexit
   fi
 }
+warn_on_error () {
+  sshexit=$1
+  if [ $sshexit -eq 0 ]; then
+    echo "step ok"
+  else
+      echo "WARN: step failed with exit code $sshexit"
+      echo "WARN: continuing deployment"
+  fi
+}
 echo "+++++ starting deployment on machine for TAG:[${TAG}]"
 if [ -z ${RAILS_MASTER_KEY} ]; then echo RAILS_MASTER_KEY missing ; else echo RAILS_MASTER_KEY exists; fi
 if [ -z ${LDAP} ]; then echo LDAP missing ; else echo LDAP exists; fi
@@ -17,7 +26,7 @@ dump_file=dumps/imi-map-$(date +%Y-%m-%d)-${TAG}.pgdump
 echo "+++++ creating database dump in ${dump_file}"
 mkdir -p dumps
 docker exec postgresql pg_dump -h localhost -U imi_map  imi_map_production > $dump_file
-exit_on_error $?
+warn_on_error $?
 echo "+++++ stopping docker containers"
 docker-compose -f docker-compose-production.yml down
 echo "+++++ pulling and starting docker containers"
