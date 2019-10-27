7f6dd049715e4531e8f8fd7de576a87f50e36655
B Kleinen <kleinen@infrastructure.de>
2019-10-27 21:23:01 +0100

    abilities refactoring II

--- app/models/abilities/user.rb
+++ app/models/abilities/user.rb
@@ -6,8 +6,10 @@ module Abilities
     include CanCan::Ability
 
     def initialize(user)
-      complete_internship(user)
-      internship(user)
+      can_create_internship(user)
+      can_show_own_internship(user)
+      can_edit_own_internship(user)
+      map(user)
       company(user)
       can :read, InternshipOffer
 
@@ -16,18 +18,29 @@ module Abilities
       can %i[read update], User, id: user.id
     end
 
-    def complete_internship(user)
-      can %i[new create internship_data], CompleteInternship
-      can %i[edit show update],
-          CompleteInternship,
-          student: { user: { id: user.id } }
+    def can_create_internship(_user)
+      can %i[new create show_own], CompleteInternship
+      can %i[create new], Internship
     end
 
-    def internship(user)
-      can :internship_data, Internship
-      can %i[create new], Internship
+    def can_show_own_internship(user)
+      can %i[show_own], CompleteInternship
+      can %i[show],
+          CompleteInternship,
+          student: { user: { id: user.id } }
+      can %i[show], Internship, student: { user: user }
+    end
+
+    def can_edit_own_internship(user)
+      can %i[edit update],
+          CompleteInternship,
+          student: { user: { id: user.id } }
+      can %i[edit update], Internship, student: { user: user }
+
       # can :update, Internship, approved: false,  student: { user: user }
-      can %i[update show], Internship, student: { user: user }
+    end
+
+    def map(_user)
       can :map_cities, Internship
     end
 
