88c11b44386da59edb3f53aee4cfc2017e9697e7
B Kleinen <kleinen@infrastructure.de>
2019-10-28 14:25:18 +0100

    all tests working again

--- spec/features/internships/05_approved_internships_cannot_be_changed_spec.rb
+++ spec/features/internships/05_approved_internships_cannot_be_changed_spec.rb
@@ -4,12 +4,31 @@ require 'rails_helper'
 
 describe 'Ability: Student edits Internship' do
   context 'not approved' do
+    it 'ensure factories are working as expecting' do
+      # this needs to work in order to assume the right associations used in
+      # the views by the tests below
+      expect { create(:internship_not_approved) }.to change {
+                                                       CompleteInternship.count
+                                                     } .by(1)
+      expect { create(:internship_not_approved) }.to change {
+                                                       Company.count
+                                                     } .by(1)
+      expect { create(:internship_not_approved) }.to change {
+                                                       CompanyAddress.count
+                                                     } .by(1)
+    end
     before :each do
-      @internship = create(:internship)
+      @internship = create(:internship_not_approved)
       @complete_internship = @internship.complete_internship
       @user = @internship.complete_internship.student.user
       sign_in @user
       visit my_internship_path
+    end
+    it 'should have complete model' do
+      ability = Ability.new(@user)
+      expect(ability.can?(:edit, @internship)).to be true
+      expect(ability.can?(:edit, @complete_internship)).to be true
+      expect(@complete_internship.student.internships.size).to eq 1
     end
     it ' shows edit link' do
       expect(page).to have_link(t('buttons.edit'), href:
@@ -19,28 +38,33 @@ describe 'Ability: Student edits Internship' do
     it 'shows personal details edit link' do
       expect(page).to have_link(
         t('complete_internships.checklist.personal_details'), href:
-      edit_complete_internship_path(id: @complete_internship.student.id,
-                                    locale: I18n.locale))
+      student_path(id: @complete_internship.student.id,
+                   locale: I18n.locale)
+      )
     end
 
     it 'shows company details edit link' do
+      save_and_open_page
       expect(page).to have_link(
         t('complete_internships.checklist.company_details'), href:
     edit_company_path(id: @internship.company_address.company,
                       internship_id: @internship.id,
-                      locale: I18n.locale))
+                      locale: I18n.locale)
+      )
     end
     it 'shows internship details as link' do
+      save_and_open_page
       expect(page).to have_link(
         t('complete_internships.checklist.internship_details'), href:
       edit_internship_path(id: @internship.id,
-                           locale: I18n.locale))
+                           locale: I18n.locale)
+      )
     end
     it 'shows contract radio buttons'
   end
   context 'approved' do
     before :each do
-      @internship = create(:internship, approved: true)
+      @internship = create(:internship)
       @complete_internship = @internship.complete_internship
       @user = @internship.complete_internship.student.user
       sign_in @user
