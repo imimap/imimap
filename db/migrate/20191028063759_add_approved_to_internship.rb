0b5f6f13eb0d999da1d8408a7c3cbd3cef7ba1ff
B Kleinen <kleinen@infrastructure.de>
2019-10-28 10:40:48 +0100

    moved migration due to merge order

--- db/migrate/20191028063759_add_approved_to_internship.rb
+++ db/migrate/20191028063759_add_approved_to_internship.rb
@@ -3,7 +3,3 @@ class AddApprovedToInternship < ActiveRecord::Migration[6.0]
     add_column :internships, :approved, :boolean, default: false
   end
 end
-
-# 20190913122135 AddFeatureTogglesToUser
-# 20191022132522 AddDefaultsForAepPassedInCompleteInternship
-# 20191027203648 CreateUserCanSeeCompanies
