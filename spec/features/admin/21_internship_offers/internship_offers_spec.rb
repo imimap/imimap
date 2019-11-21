d93073f61fb80cd2596bf4bc90921aa53e9a2967
B Kleinen <kleinen@infrastructure.de>
2019-11-21 22:10:32 +0100

    rubocop

--- spec/features/admin/21_internship_offers/internship_offers_spec.rb
+++ spec/features/admin/21_internship_offers/internship_offers_spec.rb
@@ -10,11 +10,11 @@ describe 'ActiveAdmin InternshipOffer Edit' do
     I18n.locale = 'de'
   end
   it 'edits internship_offer' do
-  #  fields = %w[street zip city phone fax latitude longitude]
+    #  fields = %w[street zip city phone fax latitude longitude]
 
     internship_offer = create(:internship_offer)
     visit edit_admin_internship_offer_path(id: internship_offer)
-  #  save_and_open_page
+    #  save_and_open_page
     uncheck 'internship_offer_active'
     click_on I18n.t('helpers.submit.update',
                     model: InternshipOffer.model_name.human)
