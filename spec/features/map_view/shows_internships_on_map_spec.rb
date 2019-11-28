7e5b48353ab1515409ad6d4ceb55e18f0b96100f
B Kleinen <kleinen@infrastructure.de>
2019-11-28 13:18:57 +0100

    test case for bug #509

--- spec/features/map_view/shows_internships_on_map_spec.rb
+++ spec/features/map_view/shows_internships_on_map_spec.rb
@@ -17,17 +17,33 @@ describe 'Map View' do
     before :each do
       @admin = create(:admin)
       sign_in(@admin)
+      @ws19 = ws19 = create(:ws2019)
+      @ss19 = ss19 = create(:ss2019)
+      @ca1 = ca1 = create(:company_address_for_edit)
+      @ca2 = ca2 = create(:company_address_1)
+      @internship1 = create(:internship_without_company_address,
+                            semester: ws19, company_address: ca1)
+      @internship2 = create(:internship_without_company_address,
+                            semester: ss19, company_address: ca2)
     end
-    it 'map is shown' do
-      ws19 = create(:ws2019)
-      ss19 = create(:ss2019)
-      ca1 = create(:company_address_for_edit)
-      ca2 = create(:company_address_1)
-      internship1 = create(:internship, semester: ws19, company_address: ca1)
-      internship2 = create(:internship, semester: ss19, company_address: ca2)
-      visit root_path
+    it "map is shown for #{@ws19}" do
+      visit root_path(semester_id: @ws19.id)
+      # expect(@internship1.company_address.city).to eq @ca1.city
       expect(page).to have_content('HTW Berlin')
-      save_and_open_page
+      expect(page.body).to have_content(@ca1.city)
+      expect(page.body).not_to have_content(@ca2.city)
+    end
+    it "map is shown for #{@ss19}" do
+      visit root_path(semester_id: @ss19.id)
+      expect(page).to have_content('HTW Berlin')
+      expect(page.body).not_to have_content(@ca1.city)
+      expect(page.body).to have_content(@ca2.city)
+    end
+    it 'map is shown all semesters ' do
+      visit root_path(semester_id: -1)
+      expect(page).to have_content('HTW Berlin')
+      expect(page.body).to have_content(@ca1.city)
+      expect(page.body).to have_content(@ca2.city)
     end
   end
 end
