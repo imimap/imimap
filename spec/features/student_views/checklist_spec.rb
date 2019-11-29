115fbe0478eac68e04b3fa8345e51d13e22a3466
B Kleinen <kleinen@infrastructure.de>
2019-11-29 12:23:24 +0100

    tests for checklist pageflow

--- spec/features/student_views/checklist_spec.rb
+++ spec/features/student_views/checklist_spec.rb
@@ -45,44 +45,48 @@ module CheckListHelper
             end
 
             context 'back to complete internship' do
-              it ' from internship details' do
-                visit my_internship_path
-
-                click_link(t('internships.internship_details'))
-                expect(page).to have_content(
-                  t('activerecord.attributes.internship.supervisor_name')
-                )
-                click_on t('save')
-                click_on t('buttons.back')
-                # click_on t('buttons.back_to_overview')
-                expect_to_be_on_my_internship_page
-              end
-
-              it ' from personal details' do
-                visit my_internship_path
-                click_link(
-                  t('complete_internships.checklist.personal_details')
-                )
-                expect(page)
-                  .to have_content(
-                    t('activerecord.attributes.student.birthday')
+              context ' from internship details' do
+                before(:each) do
+                  visit my_internship_path
+                  click_link(t('internships.internship_details'))
+                  expect(page).to have_content(
+                    t('activerecord.attributes.internship.supervisor_name')
                   )
-                click_on t('buttons.back_to_overview')
-                expect_to_be_on_my_internship_page
+                end
+                it 'without save' do
+                  click_on t('buttons.back_to_overview')
+                  expect_to_be_on_my_internship_page
+                end
+                it 'after save' do
+                  click_on t('save')
+                  # currently jumps back to overview immediately
+                  #click_on t('buttons.back')
+                  # click_on t('buttons.back_to_overview')
+                  expect_to_be_on_my_internship_page
+                end
               end
-
-              it ' from personal details after save' do
-                visit my_internship_path
-                click_link(t('complete_internships.checklist.personal_details'))
-                expect(page)
-                  .to have_content(
-                    t('activerecord.attributes.student.birthday')
+              context ' from personal details' do
+                before(:each) do
+                  visit my_internship_path
+                  click_link(
+                    t('complete_internships.checklist.personal_details')
                   )
-                click_on t('helpers.submit.generic_update')
-                click_on t('buttons.back_to_overview')
-                expect_to_be_on_my_internship_page
+                  expect(page)
+                    .to have_content(
+                      t('activerecord.attributes.student.birthday')
+                    )
+                end
+                it ' without save' do
+                  click_on t('buttons.back_to_overview')
+                  expect_to_be_on_my_internship_page
+                end
+
+                it ' from personal details after save' do
+                  click_on t('helpers.submit.generic_update')
+                  click_on t('buttons.back_to_overview')
+                  expect_to_be_on_my_internship_page
+                end
               end
-              
             end
           end
         end
