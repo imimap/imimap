cca2a0312990611dd16eed072576afa5630d895c
B Kleinen <kleinen@infrastructure.de>
2019-12-01 00:32:23 +0100

    cleaned up belongs_to (orientation) and habtm (programming_languages) field names in form and I18n

--- app/admin/internships/internships_form.rb
+++ app/admin/internships/internships_form.rb
@@ -50,10 +50,8 @@ ActiveAdmin.register Internship do
       f.input :supervisor_name
       f.input :supervisor_email
       f.input :supervisor_phone
-      f.input :programming_languages,
-              label: t('internships.attributes.programming_languages')
-      f.input :orientation,
-              label: t('internships.attributes.orientation')
+      f.input :programming_languages
+      f.input :orientation
     end
     f.inputs 'Administration' do
       f.input :approved
