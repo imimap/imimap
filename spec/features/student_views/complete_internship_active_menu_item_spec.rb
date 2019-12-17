3fe7876c6a4dbf6a7eca0fa4d78e1ef8c97746a2
B Kleinen <kleinen@infrastructure.de>
2019-12-17 02:03:40 +0100

    fixed spec

--- spec/features/student_views/complete_internship_active_menu_item_spec.rb
+++ spec/features/student_views/complete_internship_active_menu_item_spec.rb
@@ -6,7 +6,7 @@ describe 'Complete Internship' do
   I18n.available_locales.each do |locale|
     context "in locale #{locale}" do
       before :each do
-        I18n.locale = locale
+        I18n.locale = @locale = locale
         allow_ldap_login(success: false)
       end
       context 'with valid user credentials' do
@@ -16,7 +16,14 @@ describe 'Complete Internship' do
 
         it 'should mark complete internship as active' do
           visit my_internship_path
-          expect(page).to have_css('.active')
+          expect(page).to have_css('.im-nav-itemactive')
+        end
+
+        it 'should mark internship offers as active' do
+          visit internship_offers_path(locale: @locale)
+          expect(page).to have_css('.im-nav-itemactive')
+          n = find('li.im-nav-itemactive > a')
+          expect(n['href']).to eq internship_offers_path(locale: @locale)
         end
       end
     end
