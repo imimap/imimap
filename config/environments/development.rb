6049e09e6d4e9c4347ef0877429644ead85add38
B Kleinen <kleinen@infrastructure.de>
2020-01-03 23:52:28 +0100

    moved seed data to general config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,14 +1,5 @@
 # frozen_string_literal: true
 
-SeedData = Struct.new(:enrolment_number_range,
-                      :with_internships,
-                      :with_user,
-                      :with_student) do
-  def initialize(*)
-    super
-    self.with_student = true if with_student.nil?
-  end
-end
 Rails.application.configure do
   # Settings specified here will take precedence over those in
   # config/application.rb.
@@ -76,11 +67,5 @@ Rails.application.configure do
   # IMI-Maps specific begin
   config.assets.unknown_asset_fallback = false
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
-  config.seed_data = [SeedData.new(130_001..130_020, 0, true),
-                      SeedData.new(140_001..140_020, 1, true),
-                      SeedData.new(150_001..150_020, 2, true),
-                      SeedData.new(160_001..160_020, 0, true, false),
-                      SeedData.new(110_001..110_020, 0, false),
-                      SeedData.new(120_001..120_020, 1, false)]
   # IMI-Maps specific end
 end
