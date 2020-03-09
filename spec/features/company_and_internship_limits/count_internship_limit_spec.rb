85ff9dcd784c9ed3f2e9c53d8bfdef2a6e53bbe5
Lotte <lotteunckell@gmail.com>
2020-03-09 16:22:47 +0100

    added tests for UserCanSeeInternships, almost fixes code coverage

--- spec/features/company_and_internship_limits/count_internship_limit_spec.rb
+++ spec/features/company_and_internship_limits/count_internship_limit_spec.rb
@@ -19,22 +19,151 @@ describe 'Internship limit' do
 
       context 'is counted correctly' do
         before :each do
-          login_as_admin
-          create_complete_internship
+          10.times do
+            create(
+              :internship,
+              start_date: Date.today.to_date - 7.days
+            )
+          end
+          10.times do
+            create(
+              :internship_1,
+              start_date: Date.today.to_date - 7.days
+            )
+          end
+
+          @current_user = login_as_student
+          visit start_search_path
+        end
+
+        context 'when making no query' do
+          it 'is empty' do
+            expect(
+              UserCanSeeInternship
+              .number_of_viewed_internships_for_user(user: @current_user)
+            ).to equal(0)
+          end
         end
 
         context 'when making one query' do
-          it 'with one result'
+          it 'with one result' do
+            internship = create(
+              :internship_1,
+              start_date: Date.today.to_date - 7.days,
+              payment_state: nil,
+              salary: -5
+            )
+            select t('search.is_not_paid'), from: 'search_paid'
+            click_on t('search.buttons.search')
+            expect(page).to have_content(
+              t('search.results_found.start').to_s +
+              ' ' + 1.to_s + ' ' +
+              t('search.results_found.finish').to_s
+            )
+            expect(page).to have_content(
+              internship.company_address.company.name
+            )
+            expect(
+              UserCanSeeInternship
+              .number_of_viewed_internships_for_user(user: @current_user)
+            ).to equal(1)
+          end
 
-          it 'with no results'
+          it 'with no results' do
+            select t('search.is_not_paid'), from: 'search_paid'
+            click_on t('search.buttons.search')
+            expect(page).to have_content(
+              t('search.results_found.start').to_s +
+              ' ' + 0.to_s + ' ' +
+              t('search.results_found.finish').to_s
+            )
+            expect(
+              UserCanSeeInternship
+              .number_of_viewed_internships_for_user(user: @current_user)
+            ).to equal(0)
+          end
 
-          it 'with loads of results'
+          it 'with loads of results' do
+            click_on t('search.buttons.search')
+            expect(page).to have_content(
+              t('search.results_found.start').to_s +
+              ' ' + 12.to_s + ' ' +
+              t('search.results_found.finish').to_s
+            )
+            expect(
+              UserCanSeeInternship
+              .number_of_viewed_internships_for_user(user: @current_user)
+            ).to equal(12)
+          end
         end
 
         context 'when making multiple queries' do
-          it 'with same result'
+          it 'with same results' do
+            5.times do
+              create(
+                :internship_1,
+                start_date: Date.today.to_date - 7.days,
+                payment_state: nil,
+                salary: -5
+              )
+            end
+            select t('search.is_not_paid'), from: 'search_paid'
+            click_on t('search.buttons.search')
+            expect(page).to have_content(
+              t('search.results_found.start').to_s +
+              ' ' + 5.to_s + ' ' +
+              t('search.results_found.finish').to_s
+            )
+            expect(
+              UserCanSeeInternship
+              .number_of_viewed_internships_for_user(user: @current_user)
+            ).to equal(5)
 
-          it 'with different results'
+            click_on t('search.buttons.search')
+            expect(page).to have_content(
+              t('search.results_found.start').to_s +
+              ' ' + 5.to_s + ' ' +
+              t('search.results_found.finish').to_s
+            )
+            expect(
+              UserCanSeeInternship
+              .number_of_viewed_internships_for_user(user: @current_user)
+            ).to equal(5)
+          end
+
+          it 'with different results' do
+            5.times do
+              create(
+                :internship_1,
+                start_date: Date.today.to_date - 7.days,
+                payment_state: nil,
+                salary: -5
+              )
+            end
+            select t('search.is_not_paid'), from: 'search_paid'
+            click_on t('search.buttons.search')
+            expect(page).to have_content(
+              t('search.results_found.start').to_s +
+              ' ' + 5.to_s + ' ' +
+              t('search.results_found.finish').to_s
+            )
+            expect(
+              UserCanSeeInternship
+              .number_of_viewed_internships_for_user(user: @current_user)
+            ).to equal(5)
+
+            select t('search.is_paid'), from: 'search_paid'
+            click_on t('search.buttons.search')
+            expect(page).to have_content(
+              t('search.results_found.start').to_s +
+              ' ' + 7.to_s + ' ' +
+              t('search.results_found.finish').to_s
+            )
+            expect(
+              UserCanSeeInternship
+              .number_of_viewed_internships_for_user(user: @current_user)
+            ).to equal(12)
+          end
         end
       end
     end
