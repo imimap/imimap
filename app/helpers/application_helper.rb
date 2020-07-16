0ab0636e3831eeb39598af012b9014c39a83205b
thuemmels <thummel.stephan@yahoo.de>
2020-07-16 18:33:53 +0200

    closes #597

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,7 +45,7 @@ module ApplicationHelper
       locale_param = if request.path == root_path
                        root_path(locale: loc)
                      else
-                       params.permit(%i[locale internship_id company_id])
+                       params.permit(%i[locale internship_id company_id cidcontext complete_internship])
                              .merge(locale: loc)
                      end
       concat content_tag(:li, (link_to_unless_current loc, locale_param),
