c8d870848c15401ec9524232ba9b8987ed8179db
Barne Kleinen <barne.kleinen@htw-berlin.de>
2020-12-23 14:58:49 +0100

    update environment for local testing

--- ci-cd/export_environment_vars_for_testing.sh
+++ ci-cd/export_environment_vars_for_testing.sh
@@ -6,7 +6,7 @@ export DEPLOYMENT_PIPELINE=HTW
 # set by travis
 export TRAVIS_BRANCH=master
 export TRAVIS_TAG=
-export TRAVIS_COMMIT=8824486c739fb924af89e4aeec674e4295c85454
+export TRAVIS_COMMIT=31ee77683bc23a80fcc16c65c35cf0af4f294eeb
 export TRAVIS_PULL_REQUEST=false # number or false
 export TRAVIS=true
 
@@ -14,4 +14,21 @@ export TRAVIS=true
 export IMIMAPS_ENVIRONMENT=docker
 
 # set as an parameter to or directly in the script
-export DEPLOYMENT_ENVIRONMENT=production
+# export DEPLOYMENT_ENVIRONMENT=production
+export DEPLOYMENT_ENVIRONMENT=staging
+
+
+export DEPLOMENT_USER=deployer
+export DEPLOYMENT_HOST=imi-map-staging.f4.htw-berlin.de
+
+export DEPLOYMENT_TAG=$(git log --pretty=format:'%h' -n 1)
+echo $DEPLOYMENT_TAG
+
+
+export DEPLOYMENT_TAG=48a87ae
+export DEPLOYMENT_TAG=31ee776
+
+
+# for using: -remove the -i part
+# ssh  -i id_rsa_$DEPLOYMENT_ENVIRONMENT -o StrictHostKeyChecking=no $DEPLOMENT_USER@$DEPLOYMENT_HOST "export set TAG=$DEPLOYMENT_TAG; export set LDAP=${LDAP}; export set RAILS_MASTER_KEY=$RAILS_MASTER_KEY; export set GOOGLE_API_KEY=$GOOGLE_API_KEY; . ./deployment-steps-on-production-machine.sh" 2>&1
+# ssh   -o StrictHostKeyChecking=no $DEPLOMENT_USER@$DEPLOYMENT_HOST "export set TAG=$DEPLOYMENT_TAG; export set LDAP=${LDAP}; export set RAILS_MASTER_KEY=$RAILS_MASTER_KEY; export set GOOGLE_API_KEY=$GOOGLE_API_KEY; . ./deployment-steps-on-production-machine.sh" 2>&1
