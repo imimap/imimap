31ee77683bc23a80fcc16c65c35cf0af4f294eeb
Barne Kleinen <barne.kleinen@htw-berlin.de>
2020-12-23 14:38:40 +0100

    verbose migration

--- ci-cd/deployment-steps-on-production-machine.sh
+++ ci-cd/deployment-steps-on-production-machine.sh
@@ -55,9 +55,10 @@ echo "+++++ Wait for postgres"
 docker-compose -f docker-compose-production.yml exec imimap ./ci-cd/wait-for-db-connection.sh
 exit_on_error $?
 
-echo "+++++ MIGRATION_ON_DEPLOYMENT_NOT_WORKING"
+echo "+++++ Database Migration Status"
+docker-compose -f docker-compose-production.yml exec imimap rails db:migrate:status
 echo "+++++ Database Migration"
-docker-compose -f docker-compose-production.yml exec imimap bundle exec rails db:migrate
+docker-compose -f docker-compose-production.yml exec imimap rails db:migrate
 exit_on_error $?
 
 echo "+++++ done with deployment steps on production machene for TAG:[${TAG}]"
