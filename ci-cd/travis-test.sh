2bbc1e6b87547f3059e3cffd5303e5a249df5bc3
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-03-04 11:04:26 +0100

    use test repo

--- ci-cd/travis-test.sh
+++ ci-cd/travis-test.sh
@@ -7,12 +7,14 @@ echo "$0: starting build for IMIMAPS_ENVIRONMENT ${IMIMAPS_ENVIRONMENT}"
   echo "--------- sudo netstat -nlp | grep 5432"
   sudo netstat -nlp | grep 5432
 
-  export DOCKER_API_TOKEN=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:imimap/imimap:pull" | jq -r .token)
-  curl --head -H "Authorization: Bearer $DOCKER_API_TOKEN" https://registry-1.docker.io/v2/imimap/imimap/manifests/latest
+  echo "docker hub limits before build"
+  export DOCKER_API_TOKEN=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" | jq -r .token)
+  curl --head -H "Authorization: Bearer $DOCKER_API_TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest
 
   docker-compose up -d
 
-  curl --head -H "Authorization: Bearer $DOCKER_API_TOKEN" https://registry-1.docker.io/v2/imimap/imimap/manifests/latest
+  echo "docker hub limits after build"
+  curl --head -H "Authorization: Bearer $DOCKER_API_TOKEN" https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest
 
   if [ $? != 0 ]; then
     echo "ERROR: docker-compose up -d FAILED"
--- doc/deployment/docker-limits.md
+++ doc/deployment/docker-limits.md
@@ -9,6 +9,29 @@ Each build pulls various images from docker hub, this is done unauthorized.
 Step 1: Build in output of limits in ci-cd/travis-test.sh as described in
 https://docs.docker.com/docker-hub/download-rate-limit/
 
+
+RateLimit-Limit: 100;w=21600
+RateLimit-Remaining: 65;w=21600
+
+RateLimit-Limit: 100;w=21600
+RateLimit-Remaining: 68;w=21600
+
 ## Tools/New Dependency
-JQ: command line JSON processor
+
+### JQ: command line JSON processor
 https://stedolan.github.io/jq/
+
+Local installation on MacOS:
+
+    brew install jq
+
+### JWT
+
+https://github.com/mike-engel/jwt-cli
+
+Local installation on MacOS:
+
+    brew tap mike-engel/jwt-cli
+    brew install jwt-cli
+
+https://www.docker.com/blog/checking-your-current-docker-pull-rate-limits-and-status/
